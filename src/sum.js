698bcc0711cdf6b8fd4f84867f1be225fe568cde
Misha Grinko <glukym@gmail.com>
2019-06-25 16:17:00 +0300

    description was fixed

--- src/sum.js
+++ src/sum.js
@@ -3,17 +3,18 @@
 /***
  * Implement `sum` function that can be used the next way:
  *
+ * sum() === 0
  * sum(1)() === 1
- * sum(2)(3)() === 13
+ * sum(2)(3)() === 5
  * sum(1)(1)(1)(1)(1)(1)(1)(1)() === 8
  *
- * At lease 1 argument is passed. Every chain is finished with empty parens ()
+ * Every chain is finished with empty parens ()
  *
  * @param {number} a
  *
  * @return {function}
  */
-function sum() {
+function sum(a) {
   // write code here
 }
 
