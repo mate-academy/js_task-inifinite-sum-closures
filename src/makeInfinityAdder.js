05bf52cb851657b8fdcdaa9f0ab7dbd094df4bf7
Victor-Zinchenko-mate <65622349+Victor-Zinchenko-mate@users.noreply.github.com>
2020-07-07 21:45:11 +0300

    SolutionClosures2

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,21 @@
 'use strict';
 
-/***
- *
- * @return {function}
- */
-
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+  const makeAdder = (value) => {
+    if (value === undefined) {
+      const sum = result;
+
+      result = 0;
+
+      return sum;
+    };
+    result = result + value;
+
+    return makeAdder;
+  };
+
+  return makeAdder;
 }
 
 module.exports = makeInfinityAdder;
