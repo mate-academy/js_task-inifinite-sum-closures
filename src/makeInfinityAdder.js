a30d4b722e2b43713ead984003b4997579ee434b
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-08-04 16:03:46 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let count = 0;
+  const loop = (item) => {
+    if (item === undefined && count > 0) {
+      return 0;
+    }
+
+    if (item === undefined) {
+      count++;
+
+      return sum;
+    }
+
+    sum += item;
+
+    return loop;
+  };
+
+  return loop;
 }
 
 module.exports = makeInfinityAdder;
