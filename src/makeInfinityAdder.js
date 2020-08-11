8b85c4d7441033735dc0911e81f8c863af7c4e5c
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-11 20:10:46 +0300

    +

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,21 @@
 
 function makeInfinityAdder() {
   // write code here
+  let amount = 0;
+
+  return function add(value) {
+    if (value === undefined) {
+      const result = amount;
+
+      amount = 0;
+
+      return result;
+    } else {
+      amount += value;
+    }
+
+    return add;
+  };
 }
 
 module.exports = makeInfinityAdder;
