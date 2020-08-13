a73af6a4e4c0e208f3cdcb4f545eb209749a12b9
uni-8 <julia.zhol.a@gmail.com>
2020-08-13 22:02:29 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,28 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let emptyInput = 0;
+
+  const hellRecursion = (value) => {
+    if (emptyInput > 0) {
+      sum = 0;
+
+      return sum;
+    }
+
+    if (value === undefined) {
+      emptyInput++;
+
+      return sum;
+    }
+
+    sum += value;
+
+    return hellRecursion;
+  };
+
+  return hellRecursion;
 }
 
 module.exports = makeInfinityAdder;
