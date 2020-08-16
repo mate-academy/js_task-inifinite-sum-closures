9791e4f52dd25527dc62e89d3207ea849de7a93f
Anastasiia Lukina <anlukina24@gmail.com>
2020-08-16 12:45:41 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,9 +4,22 @@
  *
  * @return {function}
  */
+let sum = 0;
 
 function makeInfinityAdder() {
-  // write code here
+  return (number) => {
+    if (number) {
+      sum += number;
+
+      return makeInfinityAdder();
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
