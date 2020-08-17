9786e7eae63622a81e3b293190c0b3e46bdd3a5a
sobchuksasha <sobchuksasha1995@gmail.com>
2020-08-17 14:30:53 +0300

    ready

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const x = (operand) => {
+    if (operand !== undefined) {
+      sum += operand;
+
+      return x;
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  };
+
+  return x;
 }
 
 module.exports = makeInfinityAdder;
