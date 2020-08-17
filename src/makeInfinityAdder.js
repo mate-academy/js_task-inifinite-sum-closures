af52579f24526a5d1085b9a867cb5f43336e07a2
Gabbasova Oksana <oksana.gabba@gmail.com>
2020-08-17 23:31:47 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function addingFunction(n) {
+    if (n === undefined) {
+      const tmpSum = sum;
+
+      sum = 0;
+
+      return tmpSum;
+    }
+
+    sum += n;
+
+    return addingFunction;
+  }
+
+  return addingFunction;
 }
 
 module.exports = makeInfinityAdder;
