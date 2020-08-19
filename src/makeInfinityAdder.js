ebe877729ccc9d699109b9ccf5ae3990ddf97b21
karinaKudatska <karina.kudatska@gmail.com>
2020-08-19 12:23:16 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function adder(n) {
+    if (!n) {
+      const sum = result;
+
+      result = 0;
+
+      return sum;
+    } else {
+      result += n;
+
+      return adder;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
