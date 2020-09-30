528bcda30b5d60869b10f9d533abf36b3c351caa
Alina <alina90565@gmail.com>
2020-09-30 12:21:15 +0300

    done inifinite sum closures task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const getSum = (number) => {
+    if (!number) {
+      const currentSum = sum;
+
+      sum = 0;
+
+      return currentSum;
+    }
+
+    sum += number;
+
+    return getSum;
+  };
+
+  return getSum;
 }
 
 module.exports = makeInfinityAdder;
