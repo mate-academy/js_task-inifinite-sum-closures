dee3f7cd8c0aa0709f88e0f543fbe350d84e6e69
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-02 17:00:24 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(number) {
+    if (!number) {
+      const sumCurrent = sum;
+
+      sum = 0;
+
+      return sumCurrent;
+    };
+
+    sum += number;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
