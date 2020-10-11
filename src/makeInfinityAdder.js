cfd5da4719f768d12ed33c249095f9822c85cafd
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-11 23:47:21 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const add = (number) => {
+    if (number !== undefined) {
+      sum += number;
+
+      return add;
+    } else {
+      const finallySum = sum;
+
+      sum = 0;
+
+      return finallySum;
+    }
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
