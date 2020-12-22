0583ce679bdb4f78fe4b6b95f6f86a0896c9ace3
Valentina Vorfolomeeva <vorfolomeevav@gmail.com>
2020-12-22 20:00:13 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function adder(number) {
+    if (number !== undefined) {
+      result += number;
+
+      return adder;
+    }
+
+    const savedResult = result;
+
+    result = 0;
+
+    return savedResult;
+  };
 }
 
 module.exports = makeInfinityAdder;
