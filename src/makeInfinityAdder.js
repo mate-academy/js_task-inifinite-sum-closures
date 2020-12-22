a6940a8e78e67856ef1ea95e02075f090df45941
troospieler <troospieler1990@gmail.com>
2020-12-22 16:44:57 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let overAllsum = 0;
+  let counter = 0;
+  const caller = (x) => {
+    if (counter === 0) {
+      overAllsum = 0;
+    }
+
+    if (x === undefined) {
+      counter = 0;
+
+      return overAllsum;
+    }
+
+    counter++;
+    overAllsum += x;
+
+    return caller;
+  };
+
+  return caller;
 }
 
 module.exports = makeInfinityAdder;
