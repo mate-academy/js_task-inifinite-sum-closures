c8abab239fcdc4a38fa86db813b5b88e657e3978
vovador <vovadorko@gmail.com>
2020-12-22 14:35:24 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,28 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const numbers = [];
+
+  return (numberN) => {
+    if (!numberN) {
+      return numbers.length;
+    }
+    numbers.push(numberN);
+
+    return function lastInputOperator(numberNplus1) {
+      if (numberNplus1) {
+        numbers.push(numberNplus1);
+
+        return lastInputOperator;
+      } else {
+        const accomulatedNumbers = [...numbers];
+
+        numbers.length = 0;
+
+        return accomulatedNumbers.reduce((acc, curV) => acc + curV, 0);
+      }
+    };
+  };
 }
 
 module.exports = makeInfinityAdder;
