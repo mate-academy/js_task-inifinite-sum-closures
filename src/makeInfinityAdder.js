805290b9003fe5efebf6b79238e881fa39d48322
Dubchak Marina <dubchmar@gmail.com>
2020-12-23 18:21:29 +0200

    added solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,28 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let doReset = 0;
+
+  const nextAction = (num) => {
+    if (!num && doReset === 0) {
+      doReset++;
+
+      return sum;
+    }
+
+    if (doReset > 0) {
+      sum = 0;
+
+      return sum;
+    }
+
+    sum += num;
+
+    return nextAction;
+  };
+
+  return nextAction;
 }
 
 module.exports = makeInfinityAdder;
