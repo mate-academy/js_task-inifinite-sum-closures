fb97b0baf72d5d646ba1c06f7a0b2b03e7b0fc8a
chri911 <delightsome@bigmir.net>
2020-12-26 19:44:25 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,35 @@
 'use strict';
 
 /***
- *
  * @return {function}
+ *  * const adder = makeAdder();
+adder() === 0
+adder(4)(5)() === 9
+adder() === 0
+adder(5)(5)(5)
+adder(4)
+adder() === 19
+adder() === 0
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const otherFunction = (number) => {
+    if (number === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += number;
+
+    return otherFunction;
+  };
+
+  return otherFunction;
 }
 
 module.exports = makeInfinityAdder;
