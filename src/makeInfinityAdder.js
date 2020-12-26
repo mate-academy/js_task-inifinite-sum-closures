c104b53dfe02cb56b5ffcde7e28834b2aa28b5de
chri911 <delightsome@bigmir.net>
2020-12-26 19:46:45 +0200

    delete comments

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -2,14 +2,6 @@
 
 /***
  * @return {function}
- *  * const adder = makeAdder();
-adder() === 0
-adder(4)(5)() === 9
-adder() === 0
-adder(5)(5)(5)
-adder(4)
-adder() === 19
-adder() === 0
  */
 
 function makeInfinityAdder() {
