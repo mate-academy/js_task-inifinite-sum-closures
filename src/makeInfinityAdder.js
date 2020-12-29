e5bcb529da666ee1ddd16e6a851b920158f00ce1
Viktor Morhun <victor.morgun16@gmail.com>
2020-12-29 20:02:33 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,22 @@
 'use strict';
 
-/***
- *
- * @return {function}
- */
-
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const addFunc = (prevNum) => {
+    if (!prevNum) {
+      const answer = sum;
+
+      sum = 0;
+
+      return answer;
+    }
+    sum += prevNum;
+
+    return addFunc;
+  };
+
+  return addFunc;
 }
 
 module.exports = makeInfinityAdder;
