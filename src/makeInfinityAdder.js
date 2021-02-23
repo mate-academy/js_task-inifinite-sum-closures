d776c1b5240b0bb86ff9d90a83d960d05244e725
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-02-23 17:44:09 +0200

    solved

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,20 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function counter(number) {
+    if (number === undefined) {
+      const counted = sum;
+
+      sum = 0;
+
+      return counted;
+    }
+    sum += number;
+
+    return counter;
+  };
 }
 
 module.exports = makeInfinityAdder;
