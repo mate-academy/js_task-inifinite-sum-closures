80637704c652318ca7e85fc219dde9ce37fb10a3
Artur Shpontak <ashpontak97@gmail.com>
2021-03-01 12:22:57 +0200

    required changes

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -8,21 +8,19 @@
 function makeInfinityAdder(firstValue = 0) {
   let sumOfOperations = firstValue;
 
-  const adder = (lastValue) => {
+  return function adder(lastValue) {
     if (lastValue) {
       sumOfOperations += lastValue;
 
       return adder;
-    } else {
-      const resultOfOperations = sumOfOperations;
-
-      sumOfOperations = 0;
-
-      return resultOfOperations;
     }
-  };
 
-  return adder;
+    const resultOfOperations = sumOfOperations;
+
+    sumOfOperations = 0;
+
+    return resultOfOperations;
+  };
 }
 
 module.exports = makeInfinityAdder;
