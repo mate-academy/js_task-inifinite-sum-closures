e2292667ba833938b22885681f19c8b0aa1f86e9
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-03-13 14:12:28 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,91 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const check = (item) => {
+    if (!item) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += item;
+  };
+
+  return (a) => {
+    if (!a) {
+      return check();
+    }
+
+    sum += a;
+
+    return (b) => {
+      if (!b) {
+        return check();
+      }
+
+      sum += b;
+
+      return (c) => {
+        if (!c) {
+          return check();
+        }
+
+        sum += c;
+
+        return (d) => {
+          if (!d) {
+            return check();
+          }
+
+          sum += d;
+
+          return (e) => {
+            if (!e) {
+              return check();
+            }
+
+            sum += e;
+
+            return (f) => {
+              if (!f) {
+                return check();
+              }
+
+              sum += f;
+
+              return (g) => {
+                if (!g) {
+                  return check();
+                }
+
+                sum += g;
+
+                return (h) => {
+                  if (!h) {
+                    return check();
+                  }
+
+                  sum += h;
+
+                  return (i) => {
+                    if (!i) {
+                      return check();
+                    }
+
+                    sum += i;
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 }
 
 module.exports = makeInfinityAdder;
