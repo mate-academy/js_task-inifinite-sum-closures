af860332857897150fd9310335fc37d5bcd38e36
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-14 09:20:41 +0200

    Created function makeInfinityAdder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,21 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  return function calcSum(number) {
+    if (number === undefined) {
+      const newSum = sum;
+
+      sum = 0;
+
+      return newSum;
+    } else {
+      sum += number;
+
+      return calcSum;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
