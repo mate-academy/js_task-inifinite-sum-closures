852b81a862b7fb313481772331b47918e4f023b6
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-05-05 18:52:29 +0300

    v_1.0.0

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(...args) {
+    if (args.length === 0) {
+      const total = sum;
+
+      sum = 0;
+
+      return total;
+    }
+
+    sum += args[0];
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
