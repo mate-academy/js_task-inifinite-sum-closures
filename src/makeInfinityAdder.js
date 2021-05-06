9d48047dc8509de3aaa8d8f0c18d083a0a3e92d6
Igor Lisogor <illisogori@gmail.com>
2021-05-06 19:48:54 +0300

    infinite adder implemented

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = function(number) {
+    const temp = sum;
+
+    if (number === undefined) {
+      sum = 0;
+
+      return temp;
+    }
+    sum += number;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
