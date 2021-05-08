58d3d1c61c40482403b49745a0428f27bf6a9a67
Andrii <blackdog337.ab@gmail.com>
2021-05-08 20:54:09 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let counter = 0;
+
+  return function adder(number) {
+    const countercCopy = counter;
+
+    if (number) {
+      counter += number;
+    } else {
+      counter = 0;
+
+      return countercCopy;
+    };
+
+    return adder;
+  };
+};
 
 module.exports = makeInfinityAdder;
