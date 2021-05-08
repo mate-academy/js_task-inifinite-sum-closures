8fe48a65e0108dc553219d9afb05f4c52df3fee7
Dmitry Pukhliakov <d.puhliakov@gmail.com>
2021-05-08 23:33:08 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function adder(number) {
+    if (number) {
+      result += number;
+
+      return adder;
+    }
+
+    const lastResult = result;
+
+    result = 0;
+
+    return lastResult;
+  };
 }
 
 module.exports = makeInfinityAdder;
