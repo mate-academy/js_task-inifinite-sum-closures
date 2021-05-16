04e5cc1f0074fb7801a55b1c642706ead1c5674a
VasylPerepichai <vperepichay@gmail.com>
2021-05-16 21:05:02 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = function(a) {
+    if (arguments.length === 0) {
+      const returnValue = result;
+
+      result = 0;
+
+      return returnValue;
+    } else {
+      result += a;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
