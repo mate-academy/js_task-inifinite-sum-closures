57972d44b1ee0afc18f18c80ee32a4cbd4870606
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-06-30 13:29:43 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,24 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const adder = [];
+
+  const getAdder = (atribute) => {
+    if (atribute === undefined) {
+      const sumOfAdder
+        = adder.reduce((accumulator, current) => accumulator + current, 0);
+
+      adder.length = 0;
+
+      return sumOfAdder;
+    } else {
+      adder.push(atribute);
+
+      return getAdder;
+    }
+  };
+
+  return getAdder;
 }
 
 module.exports = makeInfinityAdder;
