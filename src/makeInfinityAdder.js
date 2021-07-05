4f02ec3e8f97d3e381b11ffbde4ac774cb524dfd
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-05 18:45:32 +0300

    task is completed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,22 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+  const adder = function(number) {
+    if (number === undefined) {
+      const sumCopy = sum;
+
+      sum = 0;
+
+      return sumCopy;
+    } else {
+      sum += number;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
