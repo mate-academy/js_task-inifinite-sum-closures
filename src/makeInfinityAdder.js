fdf3e48063d1385c8bd341ad5716bb8e184f97a4
Sergei <serheifil@gmail.com>
2021-07-06 14:13:15 +0300

    tests are passed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const add = (number) => {
+    if (number === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += number;
+
+    return add;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
