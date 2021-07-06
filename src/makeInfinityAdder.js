72a707f0e2cacfa237f6f81c35b093d129d22733
Stas Pechenyi <spechenuy@gmail.com>
2021-07-06 13:51:42 +0300

    Homework was done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  const adder = (...args) => {
+    if (args.length > 0) {
+      amount += args[0];
+
+      return adder;
+    } else {
+      const answer = amount;
+
+      amount = 0;
+
+      return answer;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
