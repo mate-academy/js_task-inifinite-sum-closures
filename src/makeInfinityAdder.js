d562b391e36ed7c81e8376fe5c980410cca1d09d
Stas Pechenyi <spechenuy@gmail.com>
2021-07-06 17:43:12 +0300

    Mentor`s issues were fixxed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -8,18 +8,18 @@
 function makeInfinityAdder() {
   let amount = 0;
 
-  const adder = (...args) => {
-    if (args.length > 0) {
-      amount += args[0];
+  const adder = (number) => {
+    if (number !== undefined) {
+      amount += number;
 
       return adder;
-    } else {
-      const answer = amount;
-
-      amount = 0;
-
-      return answer;
     }
+
+    const answer = amount;
+
+    amount = 0;
+
+    return answer;
   };
 
   return adder;
