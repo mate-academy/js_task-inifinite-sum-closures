4c11443a892c94100bc9644ed92f8e876207e532
vlepekha <vlepekha@student.ucode.world>
2021-07-07 12:16:29 +0300

    solved

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (number) => {
+    if (number) {
+      result += number;
+
+      return adder;
+    }
+
+    const sum = result;
+
+    result = 0;
+
+    return sum;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
