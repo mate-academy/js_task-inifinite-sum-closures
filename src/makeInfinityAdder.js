aa8399ae88ba226d4534fb0682b6140a9cffa5b3
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-07-08 13:20:57 +0300

    first test

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,20 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function f(first) {
+    if (first === undefined) {
+      const preResult = result;
+
+      result = 0;
+
+      return preResult;
+    }
+    result += +first;
+
+    return f;
+  };
 }
 
 module.exports = makeInfinityAdder;
