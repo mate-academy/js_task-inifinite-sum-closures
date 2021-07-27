b3ee82e14c45833c39d460cf5dcb44838663482e
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-07-27 15:22:08 +0300

    Task Done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,41 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function sum2(y) {
+    if (y !== undefined) {
+      sum += y;
+
+      return sum2;
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  }
+
+  function adder(x) {
+    if (x !== undefined) {
+      sum = x;
+
+      sum2.toString = function() {
+        return sum;
+      };
+
+      return sum2;
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
