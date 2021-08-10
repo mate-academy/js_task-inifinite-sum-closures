ff44e8d3632d9bca6bd2a1c27eca7f607ab97c7e
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-10 15:21:38 +0300

    hellish challenge was done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let currentSum = 0;
+  let clearCount = 0;
+
+  const adder = (value) => {
+    if (value === undefined) {
+      clearCount++;
+
+      if (clearCount === 2) {
+        currentSum = 0;
+        clearCount = 0;
+      }
+
+      return currentSum;
+    }
+
+    currentSum += value;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
