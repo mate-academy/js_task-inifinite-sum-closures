e380ea89626cbc301d17c85aa2fa12cf7685018a
SashaRudenko <for_london@ukr.net>
2021-08-10 15:48:27 +0300

    solution edited

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,22 +7,20 @@
 
 function makeInfinityAdder() {
   // write code here
-  const values = [];
   let sumOfValues = 0;
 
   const adder = function(value) {
     if (value !== undefined) {
-      values.push(value);
-      sumOfValues = values.reduce((accumulator, item) => accumulator + item, 0);
+      sumOfValues += value;
 
       return adder;
-    } else {
-      const sumOfValuesToReturn = sumOfValues;
-
-      sumOfValues = 0;
-
-      return sumOfValuesToReturn;
     }
+
+    const sumOfValuesToReturn = sumOfValues;
+
+    sumOfValues = 0;
+
+    return sumOfValuesToReturn;
   };
 
   return adder;
