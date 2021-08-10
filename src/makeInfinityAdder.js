f0df7741f8e52f097f5dd2d7f7215aa75c23921f
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-10 20:51:07 +0300

    done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const add = (...args) => {
+    if (!args.length) {
+      const number = sum;
+
+      sum = 0;
+
+      return number;
+    }
+
+    sum += args[0];
+
+    return add;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
