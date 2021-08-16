9e635d8876b0751ffddf3050b8e74ab8af1156f9
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-16 15:04:29 +0300

    initial solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (int) => {
+    if (!int) {
+      const copy = sum;
+
+      sum = 0;
+
+      return copy;
+    };
+
+    sum += int;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
