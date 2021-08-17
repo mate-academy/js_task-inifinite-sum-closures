9e156462340bae021dc1ba1d7fa8e0aae4b6b225
Andrii Hryhoruk <andriihryhoruk5544@gmail.com>
2021-08-17 12:28:13 +0300

    save

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let currentSum = 0;
+
+  const addition = (num) => {
+    if (num === undefined) {
+      const expressionResult = currentSum;
+
+      currentSum = 0;
+
+      return expressionResult;
+    };
+
+    currentSum += num;
+
+    return addition;
+  };
+
+  return addition;
 }
 
 module.exports = makeInfinityAdder;
