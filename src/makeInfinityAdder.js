38fe9a43a1dca98051210b5b7364487195b76b3d
Andrii-Pinchuk <altkobalt4@gmail.com>
2021-08-17 08:22:46 +0300

    Hellish challange finished

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let currentSum = 0;
+
+  const adder = (currentValue) => {
+    if (currentValue === undefined) {
+      const totalSum = currentSum;
+
+      currentSum = 0;
+
+      return totalSum;
+    }
+
+    currentSum += currentValue;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
