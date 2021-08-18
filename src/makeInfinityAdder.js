45042855a29be445d7b709c7d29c86104c854140
Clon <clonvhs@gmail.com>
2021-08-18 13:40:12 +0300

    Solution
    v1.0

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(value) {
+    if (value === undefined) {
+      const returnSum = sum;
+
+      sum = 0;
+
+      return returnSum;
+    } else {
+      sum += value;
+
+      return adder;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
