2cab6420a1cbd6c293f18d9285a3137bdc631d7a
Eugene Gnedenko <deadup4@gmail.com>
2021-08-19 17:25:18 +0300

    solution by set1one

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adds = (num) => {
+    if (num !== undefined) {
+      sum += num;
+    }
+
+    if (num === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    return adds;
+  };
+
+  return adds;
 }
 
 module.exports = makeInfinityAdder;
