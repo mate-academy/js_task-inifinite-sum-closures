2eaf6ada8a27232f0ab056a7828bf69435b549e0
Victor Shevchuk <callmevitok@gmail.com>
2021-09-25 17:09:24 +0300

    4815162342

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(input) {
+    if (input === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += input;
+
+      return adder;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
