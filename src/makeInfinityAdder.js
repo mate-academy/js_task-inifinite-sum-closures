95bee819781c162793fa3f14a6386f578f003965
Dina Samoilova <s13.dina@gmail.com>
2021-10-04 21:26:44 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  const infiniteCurry = (fn, seed) => {
+    const reduceValue = (args, seedValue) =>
+      args.reduce((acc, a) => {
+        return fn.call(fn, acc, a);
+      }, seedValue);
+    const next = (...args) => {
+      return (...x) => {
+        if (!x.length) {
+          return reduceValue(args, seed);
+        }
+
+        return next(...args, reduceValue(x, seed));
+      };
+    };
+
+    return next();
+  };
+
+  const adder = infiniteCurry((x, y) => x + y, 0);
+
+  return adder;
+};
 
 module.exports = makeInfinityAdder;
