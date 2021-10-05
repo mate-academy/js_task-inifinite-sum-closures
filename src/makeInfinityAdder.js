0ef5086705cc10c18f8ef1b4ba09744e0cedfbb4
Katerina Bovdyr <katok5673966@gmail.com>
2021-10-05 13:02:40 +0300

    commit-1

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let totalSum = 0;
+
+  function adder(x) {
+    if (!x) {
+      const ret = totalSum;
+
+      totalSum = 0;
+
+      return ret;
+    };
+
+    totalSum += x;
+
+    return adder;
+  };
+
+  return adder;
+};
 
 module.exports = makeInfinityAdder;
