351a7f63ea977c75b13e10f1df4cb302e0cfe75b
Dima Narozhnyi <dmitriy.narozhnyi@gmail.com>
2021-10-11 09:48:46 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,19 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  return function add(val1) {
+    if (val1) {
+      return (val2) => {
+        if (val2) {
+          return add(val1 + val2);
+        } else {
+          return val1;
+        }
+      };
+    }
+
+    return 0;
+  };
 }
 
 module.exports = makeInfinityAdder;
