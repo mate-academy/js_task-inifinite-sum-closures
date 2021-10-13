3ab8ded6dd4eb31ab8b1565771f187fa6bd59724
Dima Narozhnyi <dmitriy.narozhnyi@gmail.com>
2021-10-13 16:45:16 +0300

    fixes

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,9 +7,9 @@
 
 function makeInfinityAdder() {
   return function add(val1) {
-    if (val1) {
+    if (val1 !== undefined) {
       return (val2) => {
-        if (val2) {
+        if (val2 !== undefined) {
           return add(val1 + val2);
         } else {
           return val1;
