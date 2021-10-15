15219b4ce50415bfc1e34d4b101c2dab7bb8ad93
boduk <popovychbb@gmail.com>
2021-10-15 08:39:48 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,6 +6,34 @@
  */
 
 function makeInfinityAdder() {
+  let calculateSum = 0;
+  let printSum = 0;
+
+  function outerSum(num1) {
+    if (arguments.length === 0) {
+      printSum = calculateSum;
+      calculateSum = 0;
+
+      return printSum;
+    }
+
+    calculateSum += num1;
+
+    return function innerSum(num2) {
+      if (arguments.length > 0) {
+        calculateSum += num2;
+
+        return innerSum;
+      } else {
+        printSum = calculateSum;
+        calculateSum = 0;
+
+        return printSum;
+      }
+    };
+  }
+
+  return outerSum;
   // write code here
 }
 
