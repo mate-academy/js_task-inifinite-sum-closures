d51e39f1c547a1f82b0e12a29a963ae3bedb71db
Max <gorodtira51@gmail.com>
2021-10-20 02:17:54 +0300

    done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,21 @@
 
 function makeInfinityAdder() {
   // write code here
+  let acc = 0;
+
+  const adder = (num) => {
+    if (num) {
+      acc += num
+      return adder;
+    }
+    if (!num) {
+      let temp = acc;
+      acc = 0;
+      return temp;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
