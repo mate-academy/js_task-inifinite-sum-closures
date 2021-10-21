3f359753610c0b04b85cfdd384746563045d52aa
Denys Anosov <anosovdenys@gmail.com>
2021-10-21 17:16:53 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sumOfArgs = 0;
+
+  return function adder(arg) {
+    if (arg === undefined) {
+      const output = sumOfArgs;
+
+      sumOfArgs = 0;
+
+      return output;
+    }
+
+    sumOfArgs += arg;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
