0bd098b4c807d440b6c6c51751b5cd5844271dae
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-10-21 20:09:50 +0300

    === 0 added

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -9,12 +9,14 @@ function makeInfinityAdder() {
   let num = 0;
 
   return function adder(x) {
-    if (x) {
+    if (x || x === 0) {
       num += x;
 
       return adder;
     }
+
     const result = num;
+
     num = 0;
 
     return result;
