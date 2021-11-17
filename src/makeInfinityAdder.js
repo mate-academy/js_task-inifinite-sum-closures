b28b14d397483d1dc0ce0263f0c24235177bbb74
Mariia Bila <freiheit.maria@gmail.com>
2021-11-17 21:16:57 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function adder(x) {
+    if (x === undefined) {
+      return result;
+    }
+
+    return function(y) {
+      if (y === undefined) {
+        result = 0;
+
+        return x;
+      }
+
+      result = x + y;
+
+      return adder(result);
+    };
+  };
 }
 
 module.exports = makeInfinityAdder;
