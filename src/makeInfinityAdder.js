de461fce379881bb49c91c9be66c213acc1b1936
Vitaliy Voytovich <voitovichatque@gmail.com>
2021-11-18 10:44:22 +0200

    infinityAdder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let count = 0;
+
+  const sumarise = (a) => {
+    if (count > 0) {
+      sum = 0;
+    }
+
+    if (typeof a === 'number') {
+      sum += a;
+
+      return sumarise;
+    } else {
+      count++;
+
+      return sum;
+    }
+  };
+
+  return sumarise;
 }
 
 module.exports = makeInfinityAdder;
