c32935f5ba35b3553b8ba788844b07302a6d9108
Daryna Dudar <keks9ra1286@gmail.com>
2021-11-22 14:18:59 +0200

    Infinity adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (addend) => {
+    if (addend === undefined) {
+      const savedSum = sum;
+
+      sum = 0;
+
+      return savedSum;
+    }
+
+    sum += addend;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
