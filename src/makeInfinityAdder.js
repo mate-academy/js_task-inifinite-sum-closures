92d10b90817af2cb2c1b52307027285cd6ae28ae
lanebx <havchikos@gmail.com>
2021-11-22 22:03:56 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let res = 0;
+  let copyRes;
+
+  function foo(value) {
+    if (value !== undefined) {
+      res += value;
+
+      return foo;
+    } else {
+      copyRes = res;
+      res = 0;
+
+      return copyRes;
+    }
+  }
+
+  return foo;
 }
 
 module.exports = makeInfinityAdder;
