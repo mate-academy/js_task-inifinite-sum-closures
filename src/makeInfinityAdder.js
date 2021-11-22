a48c6e15ccd14ba3f99590d85f140f5217d5df08
Olha Timoshenko <o.b.timoshenko@gmail.com>
2021-11-22 20:51:12 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let cash = 0;
+
+  const adder = (value) => {
+    if (value === undefined) {
+      const result = cash;
+
+      cash = 0;
+
+      return result;
+    }
+    cash += value;
+
+    return (...args) => adder(args[0]);
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
