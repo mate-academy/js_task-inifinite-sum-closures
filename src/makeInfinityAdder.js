b4ad4e9afbd7a0232c75735917c8e928a0763e72
Maksym Leshok <max6leshok@gmail.com>
2021-12-21 13:54:21 +0200

    Done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,20 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let summ = 0;
+
+  return function recursia(value) {
+    if (value === undefined) {
+      const cont = summ;
+
+      summ = 0;
+
+      return cont;
+    }
+    summ += value;
+
+    return recursia;
+  };
 }
 
 module.exports = makeInfinityAdder;
