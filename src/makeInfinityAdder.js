02f795ca54f9b775779b9bfde02ea4845a33868c
Niki <tes.kolia@gmail.com>
2021-12-23 15:46:52 +0200

    done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,23 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  const add = (value) => {
+    if (value === undefined) {
+      const sumResult = sum;
+
+      sum = 0;
+
+      return sumResult;
+    } else {
+      sum += value;
+
+      return add;
+    }
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
