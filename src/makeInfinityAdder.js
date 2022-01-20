435980658c82ec68a0aef518ebd26d9c48adf9c9
Konstantin <ivanchenkomypost@gmail.com>
2022-01-20 21:50:01 +0200

    task is done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (...a) => {
+    if (a.length) {
+      result += +a;
+
+      return adder;
+    }
+
+    const returnValue = result;
+
+    result = 0;
+
+    return returnValue;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
