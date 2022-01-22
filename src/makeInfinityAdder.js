53bb672e114c0b61ba8c3e1c03ef536c757511ef
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-22 13:19:05 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let allsum = 0;
+
+  const adder = (argument) => {
+    if (argument === undefined) {
+      const x = allsum;
+
+      allsum = 0;
+
+      return x;
+    } else if (argument.length === 0) {
+      allsum = 0;
+
+      return adder;
+    } else {
+      allsum += argument;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
