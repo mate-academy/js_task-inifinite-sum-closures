61940ab332b9827642e876b21c52e831f2540830
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-27 09:52:27 +0200

    hahahahahahahaAAhahAHhaHAh

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,29 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const adder = (a) => {
+    let sum = 0;
+
+    if (a === undefined) {
+      return sum;
+    }
+
+    sum += a;
+
+    const add = (b) => {
+      if (b === undefined) {
+        return sum;
+      }
+
+      sum += b;
+
+      return add;
+    };
+
+    return add;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
