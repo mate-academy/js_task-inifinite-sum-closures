d2a6ba451397bf44a7d8b1c3c49f3ca8cecc26e3
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-27 13:18:53 +0200

    holy....

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (num1) => {
+    if (num1 === undefined) {
+      const lastSum = sum;
+
+      sum = 0;
+
+      return lastSum;
+    } else {
+      sum += num1;
+
+      return (num2) => {
+        return adder(num2);
+      };
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
