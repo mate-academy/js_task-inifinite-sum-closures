f6b0e7257d95d51a456b5ced1bdcbb03054de0f2
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-01-30 16:55:41 +0300

    rec

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const f = function(number) {
+    if (number === undefined) {
+      const sumInFunction = sum;
+
+      sum = 0;
+
+      return sumInFunction;
+    }
+    sum += number;
+
+    return f;
+  };
+
+  return f;
 }
 
 module.exports = makeInfinityAdder;
