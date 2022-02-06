0528fa155eb38f64e617b5f1af1ead943698a41f
Yuri Taraday <KaIIRu3@gmail.com>
2022-02-06 12:29:24 +0200

    Infinity Closure

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  function adder(...args) {
+    if (args.length < 1) {
+      const result = amount;
+
+      amount = 0;
+
+      return result;
+    }
+    amount += args.reduce((sum, number) => sum + number, 0);
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
