ba8880fb855de416f088790acab834db1f861531
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-12 21:16:33 +0200

    makeInfinityAdder function was created

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(number) {
+    if (arguments.length < 1) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += number;
+
+      return adder;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
