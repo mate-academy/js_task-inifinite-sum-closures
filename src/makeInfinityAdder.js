06214de6963547d6f375b649603caa4e3ebb77b0
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-02-16 17:57:27 +0200

    Multiple function calls

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,26 @@
 'use strict';
-
+// const add = (val) => (val ? (arg) => (arg ? add(val + arg) : val) : 0)
 /***
  *
  * @return {function}
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const add = number1 => {
+    if (number1 !== undefined) {
+      return number2 => {
+        if (number2 !== undefined) {
+          return add(number1 + number2);
+        }
+
+        return number1;
+      };
+    }
+
+    return 0;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
