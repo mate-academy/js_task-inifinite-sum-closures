8d5cd2512cdde93e5166307a11afbe9834334991
krolich500grn <edu.izaytsev@gmail.com>
2022-02-19 15:17:10 +0200

    solution hellish

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,20 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let sum = 0;
 
+  return function add(value) {
+    if (!value && value !== 0) {
+      const totalValue = sum;
+
+      sum = 0;
+
+      return totalValue;
+    }
+
+    sum += value;
+
+    return add;
+  };
+}
 module.exports = makeInfinityAdder;
