4f2bc7b10e874533c02889fc3624cd5aec1776d4
Max Shlikhta <maxok.vibe@gmail.com>
2022-02-20 13:00:42 +0200

    Adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  return function adder(num) {
+    if (num === undefined && num !== 0) {
+      const result = amount;
+
+      amount = 0;
+
+      return result;
+    }
+
+    amount += num;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
