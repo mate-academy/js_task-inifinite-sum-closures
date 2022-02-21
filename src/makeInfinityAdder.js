484acd3d00d7ef0baaa5db775c54d72bb06c1457
addKiwi <skrypak1997@gmail.com>
2022-02-21 18:01:13 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(number) {
+    if (number === undefined) {
+      const sumToReturn = sum;
+
+      sum = 0;
+
+      return sumToReturn;
+    }
+
+    sum += number;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
