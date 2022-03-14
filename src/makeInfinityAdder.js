ef05777542800321958b693b68c043a5e93f578a
Lilia Mazur <liliamazur2508@gmail.com>
2022-03-14 11:14:43 +0200

    first

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(arg) {
+    if (arg === undefined) {
+      const resultSum = sum;
+
+      sum = 0;
+
+      return resultSum;
+    };
+
+    sum += arg;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
