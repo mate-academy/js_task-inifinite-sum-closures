f9d0ebca74c0bc491058b4c6ee1b1c2b75102552
Dmytro Bychkivskyi <dbychkivskyi@cccis.com>
2022-03-21 13:06:57 +0200

    Finish hw.

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const sum = (x) => {
+    if (typeof x !== 'undefined') {
+      return (y) => {
+        if (typeof y !== 'undefined') {
+          return sum(x + y);
+        } else {
+          return x;
+        }
+      };
+    }
+
+    return 0;
+  };
+
+  return sum;
 }
 
 module.exports = makeInfinityAdder;
