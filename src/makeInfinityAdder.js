3ef88ad5d937c00b485a53e79c9cf3bd06547b4b
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-03-28 18:59:03 +0300

    js-task-infinite-sum-closures

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let counter = 0;
+
+  const adder = (value) => {
+    if (value === undefined) {
+      const result = counter;
+
+      counter = 0;
+
+      return result;
+    }
+
+    counter += value;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
