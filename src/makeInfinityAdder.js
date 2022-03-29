72dfba8466328775e5dedadbc3a1db15aae2a8dd
Alexander Skidan <xpromt@gmail.com>
2022-03-29 13:52:43 +0300

    Result

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,10 +3,37 @@
 /***
  *
  * @return {function}
+ *
+ * Example:
+```
+const adder = makeAdder();
+adder() === 0
+adder(4)(5)() === 9
+adder() === 0
+adder(5)(5)(5)
+adder(4)
+adder() === 19
+adder() === 0
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let tempSum = 0;
+
+  const adder = (n) => {
+    if (n === undefined) {
+      const result = tempSum;
+
+      tempSum = 0;
+
+      return result;
+    }
+
+    tempSum += n;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
