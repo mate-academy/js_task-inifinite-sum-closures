4b8aa416a9491227ae1da00e72f3c920e1a45f22
Max Shylenko <maxshylenko@gmail.com>
2022-04-01 17:56:25 +0200

    inifinite-sum-closures is done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,39 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+  let currentNumber;
+  let currentResult;
+
+  const adder = (x) => {
+    if (x === undefined) {
+      currentResult = result;
+      result = 0;
+
+      return currentResult;
+    }
+
+    currentNumber = x;
+    result += currentNumber;
+
+    const summ = (y) => {
+      if (y === undefined) {
+        currentResult = result;
+        result = 0;
+
+        return currentResult;
+      }
+
+      currentNumber = y;
+      result += currentNumber;
+
+      return summ;
+    };
+
+    return summ;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
