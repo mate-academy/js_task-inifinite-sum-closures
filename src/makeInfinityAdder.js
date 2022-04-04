0dcf18ee15f1d1f434e184bcb06660f1a96bc2ef
Ostap Tomashivskyi <o.tomash650416@gmail.com>
2022-04-04 11:07:42 +0300

    Fixed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,7 +7,7 @@
 
 function makeInfinityAdder(x = 0) {
   return (y) => {
-    return typeof y !== 'undefined' ? makeInfinityAdder(x + y) : x;
+    return y !== undefined ? makeInfinityAdder(x + y) : x;
   };
 }
 
