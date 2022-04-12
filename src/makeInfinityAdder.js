fdd2bad26b9c1c1860d8ab1a8c0f7959247330d5
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-04-12 14:40:29 +0300

    Create solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,20 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+  let secondEmptyCall = false;
+
+  return function adder(value) {
+    result += value || 0;
+
+    if (typeof value === 'number') {
+      return adder;
+    }
+
+    (secondEmptyCall) ? result = 0 : secondEmptyCall = true;
+
+    return result;
+  };
 }
 
 module.exports = makeInfinityAdder;
