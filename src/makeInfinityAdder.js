1d7f8a09687304fcdd30e78ad3bfe627be92e845
Helen Petrovska <paznachka@gmail.com>
2022-04-13 20:25:22 +0300

    infinityAdder was added

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  function adder(value1) {
+    if (value1 === undefined) {
+      return 0;
+    }
+
+    let sum = value1;
+
+    const cb = (value2) => {
+      if (value2 === undefined) {
+        return sum;
+      }
+      sum += value2;
+
+      return cb;
+    };
+
+    return cb;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
