d91953d356e73b7d5ba82b3e6b011fcad096d43c
yurii837 <gorogotskiy@gmail.com>
2022-04-14 19:20:38 +0300

    done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const secondAdder = (num) => {
+    if (typeof num !== 'number') {
+      const res = sum;
+
+      sum = 0;
+
+      return res;
+    } else {
+      sum = sum + num;
+
+      return secondAdder;
+    }
+  };
+
+  return secondAdder;
 }
 
 module.exports = makeInfinityAdder;
