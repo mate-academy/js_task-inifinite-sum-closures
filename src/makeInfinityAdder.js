675bd584f278aa82f0411e8c6f93b2acc8a10944
Anastasiia Sharkova <sharkova.nastik@gmail.com>
2022-04-15 17:38:57 +0200

    adder PR

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let adderSum = 0;
+
+  const adder = (digit) => {
+    const totalSum = adderSum;
+
+    if (digit === undefined) {
+      adderSum = 0;
+
+      return totalSum;
+    }
+    adderSum += digit;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
