a47aef1057685eda7467a658c7230cb26f8dc8f8
yurii837 <gorogotskiy@gmail.com>
2022-04-18 22:55:07 +0300

    after mentor review

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -9,17 +9,16 @@ function makeInfinityAdder(prev = 0) {
   let sum = prev;
 
   const secondAdder = (num) => {
-    if (typeof num !== 'number') {
+    if (num === undefined) {
       const res = sum;
 
       sum = 0;
 
       return res;
-    } else {
-      sum = sum + num;
-
-      return secondAdder;
     }
+    sum += num;
+
+    return secondAdder;
   };
 
   return secondAdder;
