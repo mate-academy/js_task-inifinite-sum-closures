0851aab9138a4d028f947289b0fbe583ea03e616
unknown <avicostov2@gmail.com>
2022-04-19 14:50:59 +0200

    solut12

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,25 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  let prev;
+
+  function adder(a) {
+    if (a === undefined) {
+      prev = sum;
+
+      sum = 0;
+
+      return prev;
+    }
+
+    sum += a;
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
