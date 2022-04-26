d45e7882368892ac95d75ba585aa79155de6b933
Anton Ustavshchykov <unton84@gmail.com>
2022-04-26 14:19:19 +0300

    fix

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,15 +7,16 @@
 
 function makeInfinityAdder() {
   let sum = 0;
-  let temp = 0;
 
   const adder = (x) => {
     if (x === undefined) {
-      temp = sum;
+      const temp = sum;
+
       sum = 0;
 
       return temp;
     }
+
     sum += x;
 
     return adder;
