320178dcf73603323ce87d28524fbc7b8cf6df7a
Artem Motalov <artemmmmmotalov@gmail.com>
2022-04-29 19:47:39 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(...param1) {
+    // check if we have empty function call => if yes - quits
+    if (param1.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    // summarize sum and add parameter
+    sum += param1[0];
+
+    // function returns itself to deploy again (until quits)
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
