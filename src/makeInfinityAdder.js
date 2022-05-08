843e93da371c8814afb8aa4dd54b8a9192235622
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-05-08 20:48:52 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,29 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let sum = 0;
+
+  return function adder(current) {
+    if (current === undefined) {
+      const clear = sum;
+
+      sum = 0;
+
+      return clear;
+    }
+
+    sum += current;
+
+    return function(input) {
+      if (input || input === 0) {
+        sum += input;
+
+        return adder;
+      } else {
+        return sum;
+      }
+    };
+  };
+};
 
 module.exports = makeInfinityAdder;
