b267c40075151f6053d994630bd91d6026424590
Daria Liubymova <dellachaise@gmail.com>
2022-05-09 02:02:35 +0300

    Implemented solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const add = (number) => {
+    if (number !== undefined) {
+      return (number2) => {
+        if (number2 !== undefined) {
+          return add(number + number2);
+        } else {
+          return number;
+        }
+      };
+    }
+
+    return 0;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
