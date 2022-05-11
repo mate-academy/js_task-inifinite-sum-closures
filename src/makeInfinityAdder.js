b259b49bcf4402666dadf55c886c4e85923c405b
koshachina <koshachina@yahoo.com>
2022-05-11 13:01:52 +0300

    infinite sum solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,25 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  const infsum = (x) => {
+    if (x || x === 0) {
+      sum += x;
+
+      return function(y) {
+        return infsum(y);
+      };
+    }
+
+    const result = sum;
+
+    sum = 0;
+
+    return result;
+  };
+
+  return infsum;
 }
 
 module.exports = makeInfinityAdder;
