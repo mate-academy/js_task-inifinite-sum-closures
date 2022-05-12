6c5803731db7be6148dc49d2d5abe6b87ce7b4e1
Andrei Sucman <sucmanandrei@gmail.com>
2022-05-12 12:45:37 +0300

    task completed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return (value) => {
+    if (arguments.length === 0) {
+      const newSum = sum;
+
+      sum = 0;
+
+      return newSum;
+    }
+
+    sum += value;
+
+    return sum;
+  };
 }
 
 module.exports = makeInfinityAdder;
