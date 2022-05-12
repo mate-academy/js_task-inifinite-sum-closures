9e372bae6434a0fe1781b95ebf65c4eef8ef8a11
elkhayate <melkhayate61@gmail.com>
2022-05-12 17:54:50 +0100

    done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,29 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (num) => {
+    const newSum = sum;
+
+    if (num !== undefined) {
+      sum += num;
+
+      return adder;
+    }
+
+    if (!num) {
+      sum = 0;
+
+      return newSum;
+    } else {
+      sum += num;
+    }
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
