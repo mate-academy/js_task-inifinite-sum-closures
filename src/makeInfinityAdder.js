3079fd7d931df2eb7e75871708db257d6aba1c98
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-05-12 13:48:54 +0300

    makeInfinityAdder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,24 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+  let counter = 0;
+
+  const adder = (num) => {
+    if (num === undefined && counter === 0) {
+      counter++;
+
+      return sum;
+    } else if (counter > 0) {
+      return 0;
+    }
+
+    sum += num;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
