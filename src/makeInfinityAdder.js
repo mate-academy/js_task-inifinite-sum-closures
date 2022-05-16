efdd4fa1fea6d102dd67d655924415c907a4b91b
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-05-16 22:47:30 +0200

    fixed suggestion

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -9,19 +9,19 @@ function makeInfinityAdder() {
   // write code here
   let total = 0;
 
-  const adder = (...args) => {
-    if (args.length === 0) {
-      let copySum = total;
+  const adder = (param) => {
+    if (param === undefined) {
+      const copySum = total;
 
       total = 0;
 
       return copySum;
     }
 
-    total += +args;
+    total += param;
 
     return adder;
-  }
+  };
 
   return adder;
 }
