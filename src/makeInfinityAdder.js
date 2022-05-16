ffa3db47a6d530dbcb484448565d257687b27c6d
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-05-16 03:05:21 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,23 @@
 
 function makeInfinityAdder() {
   // write code here
+  let total = 0;
+
+  const adder = (...args) => {
+    if (args.length === 0) {
+      let copySum = total;
+
+      total = 0;
+
+      return copySum;
+    }
+
+    total += +args;
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
