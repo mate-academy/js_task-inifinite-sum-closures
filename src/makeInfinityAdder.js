96b8e832342ae1444b6eec8b95d902f8dc476846
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-05-17 18:09:51 +0300

    fix counter

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -8,15 +8,14 @@
 function makeInfinityAdder() {
   // write code here
   let sum = 0;
-  let counter = 0;
 
   const adder = (num) => {
-    if (num === undefined && counter === 0) {
-      counter++;
+    if (num === undefined) {
+      const currentSum = sum;
 
-      return sum;
-    } else if (counter > 0) {
-      return 0;
+      sum = 0;
+
+      return currentSum;
     }
 
     sum += num;
