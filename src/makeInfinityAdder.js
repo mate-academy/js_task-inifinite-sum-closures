b6649061be7dac0036928403fe3945b5fc91074e
Vicky Miziukanova <enrikadej@gmail.com>
2022-05-17 15:46:51 +0300

    recursive is solved

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  return function adder(num) {
+    if (num === undefined) {
+      const newAmount = amount;
+
+      amount = 0;
+
+      return newAmount;
+    }
+
+    amount += num;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
