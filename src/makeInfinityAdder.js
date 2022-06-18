0341c3a80eb120d0ccd141ded15bde3358068a7e
SpaceHook <maxdumiak39@gmail.com>
2022-06-18 23:10:34 +0300

    adder - ready

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (x) => {
+    if (x !== undefined) {
+      sum += x;
+
+      return adder;
+    }
+
+    const prevSum = sum;
+
+    sum = 0;
+
+    return prevSum;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
