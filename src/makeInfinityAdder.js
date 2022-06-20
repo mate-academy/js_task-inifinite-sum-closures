48c24ca97d8526b2373497b6b65e0efa38799325
Mariia Nedilko <mary.nedilko1@gmail.com>
2022-06-20 20:20:14 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let temporarySum = 0;
+
+  return function add(input) {
+    if (typeof input === 'undefined') {
+      const temporarySumCopy = temporarySum;
+
+      temporarySum = 0;
+
+      return temporarySumCopy;
+    } else {
+      temporarySum += input;
+
+      return add;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
