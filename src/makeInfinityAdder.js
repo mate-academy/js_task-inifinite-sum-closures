fe80e7ab49c412252a9673ca3f0fc26782f81c58
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-20 12:54:17 +0300

    first commit

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,24 @@
 
 function makeInfinityAdder() {
   // write code here
+
+  const addMine = (outerValue) => {
+    if (outerValue !== undefined) {
+      const funky = (innerValue) => {
+        if (innerValue !== undefined) {
+          return addMine(outerValue + innerValue);
+        } else {
+          return outerValue;
+        }
+      };
+
+      return funky;
+    }
+
+    return 0;
+  };
+
+  return addMine;
 }
 
 module.exports = makeInfinityAdder;
