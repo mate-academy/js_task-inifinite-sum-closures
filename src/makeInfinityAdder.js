c1d02dbf7ec04c1f266e47118cee150486acbcbf
Hovera-Liubomyr <ljusuk@gmail.com>
2022-06-26 17:44:21 +0300

    makeInfinityAdder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,6 +6,20 @@
  */
 
 function makeInfinityAdder() {
+  let sum = 0;
+  let result = 0;
+
+  return function adder(arg) {
+    if (arg === undefined) {
+      result = sum;
+      sum = 0;
+
+      return result;
+    }
+    sum = sum + arg;
+
+    return adder;
+  };
   // write code here
 }
 
