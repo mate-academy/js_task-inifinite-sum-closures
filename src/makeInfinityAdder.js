88b684d9d3a272f0757d2e329f537bb373dceec2
Yura Atamaniuk <atam3905@gmail.com>
2022-07-25 16:17:18 +0300

    task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /***
  *
@@ -7,6 +7,23 @@
 
 function makeInfinityAdder() {
   // write code here
+  const addRol = (out) => {
+    if (out !== undefined) {
+      const arr = (inner) => {
+        if (inner !== undefined) {
+          return addRol(out + inner);
+        } else {
+          return out;
+        }
+      };
+
+      return arr;
+    }
+
+    return 0;
+  };
+
+  return addRol;
 }
 
 module.exports = makeInfinityAdder;
