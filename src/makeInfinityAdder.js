1772b80e7ce6f7cd8fdec374e5f0c5091b0cb7e3
Volodymyr Shymanskyi <v.shimanskyi@bnb.net.ua>
2022-07-27 15:02:23 +0300

    1-st commit

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let summ = 0;
+
+  function getSumm(x) {
+    if (x !== undefined) {
+      summ += x;
+
+      return getSumm;
+    } else {
+      const result = summ;
+
+      summ = 0;
+
+      return result;
+    }
+  }
+
+  return getSumm;
 }
 
 module.exports = makeInfinityAdder;
