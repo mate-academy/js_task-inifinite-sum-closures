547d15021ac262bd09c4b71aaf578e7801421f4c
Oleksandr Naumchak <naumchak@ukr.net>
2022-07-29 16:59:31 +0300

    JS infinite sum closures (first solution)

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let totalSum = 0;
+
+  const adder = (...args) => {
+    while (args.length !== 0) {
+      totalSum += args[0];
+
+      return adder;
+    };
+
+    const currentSum = totalSum;
+
+    totalSum = 0;
+
+    return currentSum;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
