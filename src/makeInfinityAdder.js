d03e69bef55c9579850f51a437789f3269532e4b
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-08-03 14:43:26 +0300

    Infinity Adder task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,35 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const addition = (x) => {
+    if (x === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += x;
+    }
+
+    return function(y) {
+      if (y === undefined) {
+        const result = sum;
+
+        sum = 0;
+
+        return result;
+      } else {
+        sum += y;
+
+        return addition;
+      }
+    };
+  };
+
+  return addition;
 }
 
 module.exports = makeInfinityAdder;
