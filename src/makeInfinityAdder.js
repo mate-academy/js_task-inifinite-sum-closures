535c50ec8da9e3a27c9bc095fb75ba344f469b1c
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-08-16 15:15:39 +0300

    adder working

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,23 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  function adder(x) {
+    if (x !== undefined) {
+      sum += x;
+
+      return adder;
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
