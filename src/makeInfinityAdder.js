3c855e0fcf6b3907e7d0798c43e34743fa6e2ed3
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-08-16 16:35:36 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+  let counter = 0;
+
+  const add = (x) => {
+    if (x !== undefined) {
+      result += x;
+      counter++;
+
+      return add;
+    }
+
+    if (counter > 0) {
+      counter = 0;
+
+      return result;
+    }
+
+    return 0;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
