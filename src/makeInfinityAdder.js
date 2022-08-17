8291b8b2648a437d21a397a997773f4b7a4b933e
Nikita Havryliuk <chinter.games1@gmail.com>
2022-08-17 17:33:06 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(num) {
+    if (typeof num !== 'undefined') {
+      sum += num;
+
+      return adder;
+    }
+
+    const result = sum;
+
+    sum = 0;
+
+    return result;
+  };
 }
 
 module.exports = makeInfinityAdder;
