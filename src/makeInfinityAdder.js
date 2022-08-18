107f4d9c14b749527631c9ec533ebfa124e8613a
Dima Korol <korovaidima@gmail.com>
2022-08-18 13:58:30 +0200

    changes

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -9,15 +9,15 @@ function makeInfinityAdder() {
   let result = 0;
 
   const sum = (num) => {
-
-    if (num || num === 0) {
+    if (num !== undefined) {
       result += num;
+
       return sum;
     } else {
       const current = result;
 
       result = 0;
-  
+
       return current;
     }
   };
