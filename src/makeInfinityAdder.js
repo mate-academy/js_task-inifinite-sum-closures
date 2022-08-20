373d273dc1b54fe460007632887472bc3c23521e
Shahmatov David <davidsahmatov4@gmail.com>
2022-08-20 20:43:06 +0300

    Version 0.1

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,21 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  return function adder(value) {
+    if (arguments.length === 0) {
+      const valueToReturn = sum;
+
+      sum = 0;
+
+      return valueToReturn;
+    };
+
+    sum += value;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
