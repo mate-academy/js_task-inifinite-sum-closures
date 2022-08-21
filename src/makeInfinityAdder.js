4946395d193b7d10541291b740ff05b4273c0aa5
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-08-21 11:35:01 +0300

    Fucn makeInfinityAdder implemented

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let toClear = false;
+  let result = 0;
+
+  const sum = (add) => {
+    if (toClear) {
+      result = 0;
+      toClear = false;
+    }
+
+    if (add !== undefined) {
+      result += add;
+
+      return sum;
+    }
+
+    toClear = true;
+
+    return result;
+  };
+
+  return sum;
 }
 
 module.exports = makeInfinityAdder;
