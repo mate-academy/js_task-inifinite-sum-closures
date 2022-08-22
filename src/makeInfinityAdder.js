fa451d127ecaeaa8c66cdfdbd1c22f4026a1c9f6
Dima Kobzar <kobzar7dima@gmail.com>
2022-08-22 23:33:31 +0300

    Draft summation added

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,8 +5,42 @@
  * @return {function}
  */
 
+//  const adder = makeAdder();
+//  adder() === 0
+//  adder(4)(5)() === 9
+//  adder() === 0
+//  adder(5)(5)(5)
+//  adder(4)
+//  adder() === 19
+//  adder() === 0
+
 function makeInfinityAdder() {
-  // write code here
+  let res = 0;
+
+  return function adder(params) {
+    // console.log(params);
+
+    res += params;
+
+    // console.log(res);
+
+    // return adder;
+
+    return res;
+  };
+
 }
+
+// const adder = makeInfinityAdder();
+
+// console.log( adder(4)(5)(1)(5));
+
+// console.log( adder());
+// console.log(adder(4)(5)());
+// console.log(adder());
+// console.log(adder(5)(5)(5));
+// console.log(adder(4));
+// console.log(adder());
+// console.log(adder(0));
 
 module.exports = makeInfinityAdder;
