f968ec2fca18ef01f9f9c4a3ca64f7ff9137b7e2
Max Kravchenko <max.kravchenko@gmail.com>
2022-08-23 17:41:18 +0300

    Make Infinity Adder func

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let initialResult = 0;
+  let finalAmount = 0;
+
+  function add(x) {
+    if (x !== undefined) {
+      initialResult += x;
+
+      return add;
+    }
+
+    finalAmount = initialResult;
+    initialResult = 0;
+
+    return finalAmount;
+  }
+
+  return add;
+};
 
 module.exports = makeInfinityAdder;
