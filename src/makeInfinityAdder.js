7e371401102b686329c790ed16ced83ca1630912
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-08-23 21:13:54 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  function adder(number) {
+    if (typeof number !== 'undefined') {
+      result += number;
+    }
+
+    if (typeof number === 'undefined') {
+      const temp = result;
+
+      result = 0;
+
+      return temp;
+    }
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
