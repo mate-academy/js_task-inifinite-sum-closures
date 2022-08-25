8c421bd01d7aa32adb016d2fc304edc57ead0aed
Artemii Pavlenko <artemiipav@gmail.com>
2022-08-25 12:25:49 +0300

    adder created

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(numbers) {
+    if (typeof numbers === 'undefined') {
+      const resultSum = sum;
+
+      sum = 0;
+
+      return resultSum;
+    } else {
+      sum += numbers;
+
+      return adder;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
