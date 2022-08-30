1e160a2b33b6aa364d4c67e5743a05b3e8111c76
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-08-30 14:15:18 +0800

    task done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,26 @@
 'use strict';
-
 /***
  *
  * @return {function}
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let summ = 0;
+  const adder = function(x) {
+    if (x === undefined) {
+      const k = summ;
+
+      summ = 0;
+
+      return k;
+    } else {
+      summ += x;
+    }
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
