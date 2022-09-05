9330811c549272d43a97890ef46d3adef94a33aa
HusakOleh <husakoleh91@gmail.com>
2022-09-05 14:45:51 +0300

    did the add function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,29 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let adderInput = [0];
+
+  const adder = (n) => {
+    if (n === undefined) {
+      const res = adderInput.reduce((total, el) => {
+        return total + el;
+      }, 0);
+
+      adderInput = [0];
+
+      return res;
+    };
+
+    adderInput.push(n);
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
+
+// const adder = makeInfinityAdder();
+//
+// const actual = adder(1)(10)();
