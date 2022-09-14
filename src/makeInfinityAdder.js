bd1e4010b6f058b9a2faa03ed821f32b45cb2344
Max Bovtun <maxbovtun@gmail.com>
2022-09-14 11:44:50 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,9 +4,36 @@
  *
  * @return {function}
  */
-
+// - Really wired way to calculate
+// |-- the sum of numbers, do you agree?))
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  function adder(x) {
+    total += x;
+
+    const infinityAdder = (y) => {
+      if (typeof y === 'number') {
+        total += y;
+
+        return infinityAdder;
+      } else {
+        const result = total;
+
+        total = 0;
+
+        return result;
+      }
+    };
+
+    if (x) {
+      return infinityAdder;
+    } else {
+      return 0;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
