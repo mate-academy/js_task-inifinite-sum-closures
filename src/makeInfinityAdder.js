e73a0f9a9b3d098a8d9f30c38a041a5903eee1df
Max Bovtun <maxbovtun@gmail.com>
2022-09-16 12:23:14 +0300

    fixed after review

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -10,26 +10,16 @@ function makeInfinityAdder() {
   let total = 0;
 
   function adder(x) {
-    total += x;
+    if (x || typeof x === 'number') {
+      total += x;
 
-    const infinityAdder = (y) => {
-      if (typeof y === 'number') {
-        total += y;
-
-        return infinityAdder;
-      } else {
-        const result = total;
-
-        total = 0;
-
-        return result;
-      }
-    };
-
-    if (x) {
-      return infinityAdder;
+      return adder;
     } else {
-      return 0;
+      const beforeReset = total;
+
+      total = 0;
+
+      return beforeReset;
     }
   }
 
