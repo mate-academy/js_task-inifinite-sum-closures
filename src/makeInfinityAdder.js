89722eb785dd67ab5650832ecd6dfcf83d4dd9de
grafit-off <graf.off.it@gmail.com>
2022-09-17 23:13:50 +0300

    feat: makeInfinityAdder function was implemented

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,23 @@
 
 function makeInfinityAdder() {
   // write code here
+  let res = 0;
+
+  const sum = (a) => {
+    if (a === undefined) {
+      const currentRes = res;
+
+      res = 0;
+
+      return currentRes;
+    }
+
+    res += a;
+
+    return sum;
+  };
+
+  return sum;
 }
 
 module.exports = makeInfinityAdder;
