644961525c7b68ab71042c68e9f09339cb28b8b6
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-20 13:21:07 +0300

    hell was passed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,29 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let startValue = 0;
+
+  const adder = (valueFromUser) => {
+    if (valueFromUser === undefined) {
+      startValue = 0;
+
+      return startValue;
+    }
+
+    return (nextValueFromUser) => {
+      if (nextValueFromUser === undefined) {
+        startValue = 0;
+
+        return valueFromUser;
+      }
+
+      startValue = valueFromUser + nextValueFromUser;
+
+      return adder(startValue);
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
