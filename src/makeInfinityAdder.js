7ea8437a8db721935e9894a4317a3ff34eb9d418
Serhii Hluhovskyi <glugovsky@proton.me>
2022-09-23 17:44:46 +0300

    makeInfinityAdder function was created

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  const add = (currentNumber) => {
+    if (currentNumber !== undefined) {
+      total += currentNumber;
+
+      return add;
+    }
+
+    const currentTotal = total;
+
+    total = 0;
+
+    return currentTotal;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
