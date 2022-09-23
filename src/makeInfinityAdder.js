38805dd88b48561108f46e153f7b3a88e6dca028
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-09-23 21:16:43 +0300

    function was implemented

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (value) => {
+    if (value === undefined && sum === 0) {
+      return 0;
+    }
+
+    if (value === undefined && sum !== 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += value;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
