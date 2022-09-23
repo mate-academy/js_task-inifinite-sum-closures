9e4084742862c498c718d7bf794ea37df49ae336
Datsenko Mariia <datsenko.mate@gmail.com>
2022-09-23 20:05:51 +0300

    infinity adder has been created

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (value) => {
+    if (value === undefined) {
+      const sumTotal = sum;
+
+      sum = 0;
+
+      return sumTotal;
+    }
+    sum += value;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
