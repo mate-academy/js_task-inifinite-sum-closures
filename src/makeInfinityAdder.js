5d57bdf327a1b4a467398464a6457d75157e0ab5
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-09-23 18:09:40 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,20 @@
 
 function makeInfinityAdder() {
   // write code here
+  let temp = 0;
+
+  return function adder(args) {
+    if (isNaN(args)) {
+      const sum = temp;
+
+      temp = 0;
+
+      return sum;
+    }
+    temp += args;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
