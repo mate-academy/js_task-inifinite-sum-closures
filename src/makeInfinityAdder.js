1d290bb55ecf3ba31a03f880892f1666a42af0a1
Hlazman <glazman.b@gmail.com>
2022-09-25 13:21:04 +0300

    made function makeInfinityAdder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let result = 0;
+
+  const adder = (num) => {
+    if (num === undefined) {
+      result = sum;
+      sum = 0;
+
+      return result;
+    }
+
+    sum += num;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
