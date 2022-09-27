7ebbf33cfce7a1c32a36a5134d5733bbc5a14410
Baran andiç <gizemeceylmaz@gmail.com>
2022-09-27 18:59:20 +0300

    infinite

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let sum = 0;
+
+  const adder = (num) => {
+    if (typeof num === 'undefined') {
+      const lastSum = sum;
+
+      sum = 0;
+
+      return lastSum;
+    } else {
+      sum += num;
+
+      return adder;
+    }
+  };
+
+  return adder;
+};
 
 module.exports = makeInfinityAdder;
