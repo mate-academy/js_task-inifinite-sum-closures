5effc1e2827339b2f410987434a2a0384a8e4aa9
Kolya Movchan <kmovchan261@gmail.com>
2022-09-28 20:23:24 +0300

    -inifinite-sum-closures

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let sum = 0;
+  let count = 0;
+
+  const adder = (number) => {
+    if (count && !number) {
+      sum = 0;
+    }
+
+    if (number >= 0) {
+      sum += number;
+    } else {
+      count++;
+
+      return sum;
+    };
+
+    return adder;
+  };
+
+  return adder;
+};
 
 module.exports = makeInfinityAdder;
