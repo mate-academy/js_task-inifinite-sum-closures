cdc33c382105b5ef44fab05c42e5b571257b7dd8
Maksym Storozhuk <hitthemax111@gmail.com>
2022-09-30 22:49:53 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (x) => {
+    if (typeof x === 'undefined') {
+      const returnResult = result;
+
+      result = 0;
+
+      return returnResult;
+    }
+
+    result += x;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
