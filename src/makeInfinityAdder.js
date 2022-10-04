740687a89187503678b043994cddff9d4681006d
Sergey Vorobey <sergey88nik@gmail.com>
2022-10-04 17:46:32 +0300

    Task inifinite sume closures solved

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let currentSum = 0;
+
+  function makeAdder(a) {
+    if (a !== undefined) {
+      sum += a;
+
+      return makeAdder;
+    } else {
+      currentSum = sum;
+      sum = 0;
+
+      return currentSum;
+    }
+  }
+
+  return makeAdder;
 }
 
 module.exports = makeInfinityAdder;
