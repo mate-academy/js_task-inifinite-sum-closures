81326221fa7bd3c939be0ff6667a54ec08e18d51
John Rust <johnrust12@gmail.com>
2022-10-05 16:58:10 +0300

    Sulution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 /***
@@ -6,7 +7,33 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (value) => {
+    if (value === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += value;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
+
+// Write the makeAdder function, which will
+// return the adder function. The adder
+// function should work on the following
+// pattern: adder(2)(3)(4)(1)(2)(3)(4)() === 19.
+//  It will sum all the numbers passed in
+//   the parameters until it encounters a
+//   call without parameters. When calling
+//   without parameters, it will return the
+//   result and clear the amount.
