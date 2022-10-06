653c61e22238a3f28368f89f561c6c48236dde1b
John Rust <johnrust12@gmail.com>
2022-10-06 11:44:25 +0300

    Solution1

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -10,15 +10,15 @@ function makeInfinityAdder() {
   let sum = 0;
 
   const adder = (value) => {
-    if (value === undefined) {
+    if (value !== undefined) {
+      sum += value;
+    } else {
       const result = sum;
 
       sum = 0;
 
       return result;
     }
-
-    sum += value;
 
     return adder;
   };
@@ -27,13 +27,3 @@ function makeInfinityAdder() {
 }
 
 module.exports = makeInfinityAdder;
-
-// Write the makeAdder function, which will
-// return the adder function. The adder
-// function should work on the following
-// pattern: adder(2)(3)(4)(1)(2)(3)(4)() === 19.
-//  It will sum all the numbers passed in
-//   the parameters until it encounters a
-//   call without parameters. When calling
-//   without parameters, it will return the
-//   result and clear the amount.
