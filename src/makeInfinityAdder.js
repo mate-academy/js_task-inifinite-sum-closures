abd6fe401b3cc0bb730fcbe66b465e0b4cefebd3
Bohdan Rudenko <RudBeee@gmail.com>
2022-10-30 17:28:04 +0200

    vol_!

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,28 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+  let cleanResult = 0;
+
+  const adder = (x) => {
+    if (x >= 0) {
+      result += x;
+    }
+
+    if (x === undefined && result > 0) {
+      cleanResult++;
+
+      return cleanResult === 2 ? 0 : result;
+    }
+
+    if (x === undefined && result === 0) {
+      return 0;
+    }
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
