481003feb4e207547ba9d1defd4451dd0a5bcd81
Max Fedak <max.fedak@gmail.com>
2022-10-31 12:49:28 +0200

    solotion

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,19 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  return function adder(num) {
+    if (typeof num === 'undefined') {
+      return 0;
+    }
+
+    return function(num2) {
+      if (num2 || num2 === 0) {
+        return adder(num + num2);
+      }
+
+      return num;
+    };
+  };
 }
 
 module.exports = makeInfinityAdder;
