b68119c71ed3bae4df7dea04a911753d1c68eb4e
Masha <teenderrr@gmail.com>
2022-11-01 18:04:55 +0200

    curry func

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,29 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  return (a) => {
+    let sum = a;
+
+    if (!a) {
+      return 0;
+    }
+
+    const getSum = (b) => {
+      if (b >= 0) {
+        sum += b;
+
+        return getSum;
+      }
+
+      const total = sum;
+
+      sum = 0;
+
+      return total;
+    };
+
+    return getSum;
+  };
 }
 
 module.exports = makeInfinityAdder;
