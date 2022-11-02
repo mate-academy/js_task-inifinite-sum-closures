79730431ada4c56991211e800c0e10e41248169d
Svitlana Yudina <osbka31@gmail.com>
2022-11-02 18:39:58 +0200

    first commit

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  const adder = (num) => {
+    if (num !== undefined) {
+      sum += num;
+
+      return adder;
+    }
+
+    const result = sum;
+
+    sum = 0;
+
+    return result;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
