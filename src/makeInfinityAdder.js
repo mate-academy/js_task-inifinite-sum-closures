e7bb376d4e3993f6261e8721b329d14d1a0031a3
Spirit <baolt.king@gmail.com>
2022-11-03 13:30:15 +0200

    complete

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let accamulate = 0;
+
+  const f = (adder = null) => {
+    if (adder !== null) {
+      accamulate += adder;
+
+      return f;
+    } else {
+      const answer = accamulate;
+
+      accamulate = 0;
+
+      return answer;
+    }
+  };
+
+  return f;
 }
 
 module.exports = makeInfinityAdder;
