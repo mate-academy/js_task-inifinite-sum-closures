19a66bd2b84e3800ae46de5f4714c0ca2c647b3f
Sergey Mironenko <serg1prime@gmail.com>
2022-11-06 00:18:44 +0200

    Task was done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,17 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const next = (...args) => {
+    return (x) => {
+      if (x === undefined) {
+        return args.reduce((sum, a) => sum + a, 0);
+      }
+
+      return next(...args, x);
+    };
+  };
+
+  return next();
 }
 
 module.exports = makeInfinityAdder;
