88e45073cda8160e5d43f0b3455708c0c09f64d7
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2022-11-11 08:54:09 +0100

    commit

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,17 @@
 
 function makeInfinityAdder() {
   // write code here
+
+  let result = 0;
+
+  return (a) => {
+    if (arguments.length === 0) {
+      return 0;
+    }
+    result += a + result;
+
+    return result;
+  };
 }
 
 module.exports = makeInfinityAdder;
