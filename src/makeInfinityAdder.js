2a31d5fc98847d423dc192b059421a45afff85f6
Kukharuk <its.up.to.you.dk@gmail.com>
2022-11-15 10:26:42 +0200

    infinityAdderDK

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,28 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let count = 0;
+  let sum = 0;
+
+  return function ad(x) {
+    if (x || x === 0) {
+      sum += x;
+
+      return ad;
+    } else {
+      count++;
+
+      if (count === 1) {
+        return sum;
+      }
+
+      if (count > 1) {
+        sum = 0;
+
+        return sum;
+      }
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
