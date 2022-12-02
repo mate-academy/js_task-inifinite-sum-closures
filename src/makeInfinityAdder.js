d5e23b100c31675e9b80ace7dda0020a95a3e4dd
Lefteris Loizides <loizides.lef@gmail.com>
2022-12-02 20:17:43 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let tempSum = 0;
+  let sum;
+
+  return function add(n) {
+    if (isNaN(n)) {
+      sum = tempSum;
+      tempSum = 0;
+
+      return sum;
+    }
+
+    tempSum += n;
+
+    return add;
+  };
 }
 
 module.exports = makeInfinityAdder;
