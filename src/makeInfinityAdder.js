5a3c616e79976d40edcc53b7260ba481df2132f8
Oleksii Mishchenko <alex.mishch@gmail.com>
2022-12-09 00:32:17 +0200

    makeInfinityAdder function was written

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let accumulator = 0;
+
+  const makeAdder = (x) => {
+    const sum = accumulator;
+
+    if (x || x === 0) {
+      accumulator += x;
+
+      return makeAdder;
+    }
+
+    accumulator = 0;
+
+    return sum;
+  };
+
+  return makeAdder;
+};
 
 module.exports = makeInfinityAdder;
