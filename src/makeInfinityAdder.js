79d4a8a503b9345342fdfcb88c60261961d5c5a2
Levente Krotos <krotos.levi@gmail.com>
2022-12-08 11:56:00 +0100

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function add(num) {
+    if (num === undefined) {
+      const saveValue = result;
+
+      result = 0;
+
+      return saveValue;
+    }
+
+    result += num;
+
+    return add;
+  };
 }
 
 module.exports = makeInfinityAdder;
