fe68d0827a230a9c3da53f7490562c31c3c58430
kreisberg <anastasia.kreisberg@yahoo.com>
2022-12-08 23:54:25 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (number) => {
+    if (number !== undefined) {
+      result += number;
+
+      return adder;
+    }
+
+    const totalResult = result;
+
+    result = 0;
+
+    return totalResult;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
