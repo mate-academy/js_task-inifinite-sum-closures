de0b62dbfaf0dc35439fe521012450f2e5474bfe
OLEXANDRA KOROL <sandrakorol3@gmail.com>
2022-12-08 18:22:45 +0200

    solutin

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (value) => {
+    const setValue = sum;
+
+    if (!value && value !== 0) {
+      sum = 0;
+
+      return setValue;
+    }
+    sum += value;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
