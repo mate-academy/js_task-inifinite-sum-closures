d27499136870368c184d8ac8815e7bda0614f0da
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2022-12-09 17:12:27 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,9 +4,24 @@
  *
  * @return {function}
  */
-
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const makeAdder = (element) => {
+    if (element !== undefined) {
+      sum += element;
+
+      return makeAdder;
+    } else {
+      const lastSum = sum;
+
+      sum = 0;
+
+      return lastSum;
+    }
+  };
+
+  return makeAdder;
 }
 
 module.exports = makeInfinityAdder;
