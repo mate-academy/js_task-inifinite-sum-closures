829acede31694affab2c57702abc9f86391cd2cd
JRijo <99342891+John-Stiff@users.noreply.github.com>
2022-12-09 17:37:39 +0000

    implemented makeInfinityAdder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,24 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let currentResult = 0;
+
+  return function sumFactory(inputNumber) {
+    // accepting 0, decimals and string numbers as params:
+    const isNumber = Number.isInteger(parseInt(inputNumber));
+
+    if (!isNumber) {
+      const finalResult = currentResult;
+
+      currentResult = 0;
+
+      return finalResult;
+    }
+
+    currentResult += +inputNumber;
+
+    return sumFactory;
+  };
 }
 
 module.exports = makeInfinityAdder;
