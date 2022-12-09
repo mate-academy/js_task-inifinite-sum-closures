6e34c0a989b2cff49d08fd3a308ea80c2e198d12
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2022-12-09 15:24:44 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,19 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  return function adder(a) {
+    if (a >= 0) {
+      return function(b) {
+        if (b >= 0) {
+          return adder(a + b);
+        }
+
+        return a;
+      };
+    }
+
+    return 0;
+  };
 }
 
 module.exports = makeInfinityAdder;
