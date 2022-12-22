bc75c1a4b88f822788a0f9abcd170ddb676a5b17
Kenneth Delgado <kenvastolord@gmail.com>
2022-12-22 12:58:46 +0100

    remove comments

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,7 +5,6 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
   let sum = 0;
 
   const adder = (num) => {
@@ -25,23 +24,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-// Write the `makeAdder` function, which will return the` adder` function.
-// The `adder` function should work on the following pattern:
-// `adder(2)(3)(4)(1)(2)(3)(4)() === 19`.
-// It will sum all the numbers passed in the parameters
-// until it encounters a call without parameters.
-// When calling without parameters,
-// it will return the result and clear the amount.
-//
-// Example:
-// ```
-// const adder = makeAdder();
-// adder() === 0
-// adder(4)(5)() === 9
-// adder() === 0
-// adder(5)(5)(5)
-// adder(4)
-// adder() === 19
-// adder() === 0
-// ```
 module.exports = makeInfinityAdder;
