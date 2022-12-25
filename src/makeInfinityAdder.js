52e670b1633226bf1392859027acb69c6e81bda4
haduigon <haduigon@gmail.com>
2022-12-25 21:04:02 +0200

    first commit

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,8 +5,44 @@
  * @return {function}
  */
 
-function makeInfinityAdder() {
+function makeInfinityAdder(number) {
   // write code here
+
+  let result = 0;
+
+  let count2 = 0;
+
+  const final = (val) => {
+    let count = val;
+
+    if (val || val === 0) {
+      return (arg) => {
+        if (arg || arg === 0) {
+          count += arg;
+
+          result = count;
+
+          return final(count);
+        } else {
+          count2++;
+
+          result = 0;
+
+          return count;
+        }
+      };
+    }
+
+    if (count2 > 0) {
+      result = 0;
+
+      return 0;
+    }
+
+    return result;
+  };
+
+  return final;
 }
 
 module.exports = makeInfinityAdder;
