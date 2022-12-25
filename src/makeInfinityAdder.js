0715b7b3354e03db057e699d84d5b8bf3c431a6a
yana-kotsulym <yanak7170@gmail.com>
2022-12-25 18:13:06 +0100

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (...args) => {
+    const count = sum;
+
+    if (args.length > 0) {
+      sum += args[0];
+
+      return adder;
+    } else {
+      sum = 0;
+
+      return count;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
