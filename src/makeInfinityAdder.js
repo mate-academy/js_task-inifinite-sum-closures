ca9803d71457fa483525cb0521a5033fac864af1
Iryna Pysanko <malva37@gmail.com>
2022-12-29 16:20:17 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (a) => {
+    if (a === undefined) {
+      const tmp = sum;
+
+      sum = 0;
+
+      return tmp;
+    } else {
+      sum += a;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
