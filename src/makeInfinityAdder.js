544ec07c7e029add3f54fbbd278c278a76d92dd7
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-01-02 16:16:03 +0000

    --no-edit

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let counter = 0;
+  let finalSum;
+
+  const infiniteAdder = (...args) => {
+    if (args.length) {
+      counter += Number([...args]);
+
+      return infiniteAdder;
+    }
+
+    finalSum = counter;
+    counter = 0;
+
+    return finalSum;
+  };
+
+  return infiniteAdder;
 }
 
 module.exports = makeInfinityAdder;
