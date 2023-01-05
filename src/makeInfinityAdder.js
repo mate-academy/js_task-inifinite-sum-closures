65caf2f10c14c47371651dc21180aef43d77f967
Olha Yevstifieieva <hob.bilbo@gmail.com>
2023-01-05 18:21:33 +0200

    Add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function add(variable) {
+    if (variable === undefined) {
+      const finishSum = sum;
+
+      sum = 0;
+
+      return finishSum;
+    }
+
+    sum += variable;
+
+    return add;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
