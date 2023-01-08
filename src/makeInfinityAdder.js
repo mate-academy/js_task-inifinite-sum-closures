ff75bc43cceac5ed5bbb627435b80293a6474484
PolinaSukhorukova <sukhorukovapolina03@gmail.com>
2023-01-08 16:48:51 +0200

    Infinity adding solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adding = (x) => {
+    if (x === undefined) {
+      const currentSum = sum;
+
+      sum = 0;
+
+      return currentSum;
+    }
+
+    sum += x;
+
+    return adding;
+  };
+
+  return adding;
 }
 
 module.exports = makeInfinityAdder;
