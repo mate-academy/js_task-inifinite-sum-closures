31ef797a7d50ec97feb4c5ae944d08f3a0c7b17e
bart6man <igor.musson.55@gmail.com>
2023-01-09 21:50:29 +0200

    Task was solved

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let storage = [];
+
+  const addition = (value) => {
+    if (value >= 0) {
+      storage.push(value);
+
+      return addition;
+    } else {
+      const res = storage.reduce((acc, item) => acc + item, 0);
+
+      storage = [];
+
+      return res;
+    }
+  };
+
+  return addition;
 }
 
 module.exports = makeInfinityAdder;
