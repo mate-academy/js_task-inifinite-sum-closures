9e90bbe46d7b867d26ab243f92e5e60fccf348ff
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-10 18:58:45 +0200

    task done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  const adder = (...number) => {
+    if (number.length) {
+      sum += number[0];
+
+      return adder;
+    }
+
+    const value = sum;
+
+    sum = 0;
+
+    return value;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
