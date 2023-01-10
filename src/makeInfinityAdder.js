5aa34568ff2fe81d5bfabe8e6db63ef0084dddd7
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-10 22:21:28 +0200

    Hell

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (variables) => {
+    if (variables === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += variables;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
