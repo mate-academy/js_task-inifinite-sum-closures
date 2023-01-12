8531ae3bc7edfe5108a92a9931b1ffa18e25aa78
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-12 13:14:29 +0200

    change condition

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,9 +7,9 @@
 
 function makeInfinityAdder() {
   let sum = 0;
-  const adder = (...number) => {
-    if (number.length) {
-      sum += number[0];
+  const adder = (number) => {
+    if (number !== undefined) {
+      sum += number;
 
       return adder;
     }
