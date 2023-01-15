c4bbb4faecb2bc8347d79e2b86d9239984d6afe0
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2023-01-15 23:34:47 +0200

    completed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,21 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+  const adder = (num) => {
+    if (num === undefined) {
+      const copySum = sum;
+
+      sum = 0;
+
+      return copySum;
+    }
+    sum += num;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
