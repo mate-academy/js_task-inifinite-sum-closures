ae1606bfcd87e66688e32a6bb4e723c02afef95d
stetsenko_viktoriia <stetsenko.viktoriia@gmail.com>
2023-01-23 20:00:37 +0200

    infinity adder was made

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (args) => {
+    if (args === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += args;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
