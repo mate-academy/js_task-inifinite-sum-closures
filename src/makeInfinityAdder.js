ed2956ff7bd636318f0c50d7cfd999de9f1ad376
Kate <kate@Kates-MacBook-Pro-16.local>
2023-01-26 17:26:47 +0100

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(number) {
+    if (arguments.length === 0) {
+      const temp = sum;
+
+      sum = 0;
+
+      return temp;
+    } else {
+      sum += number;
+
+      return adder;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
