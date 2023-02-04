f4356bc86b0a3f8deb01df917300b1d83f43ef1d
bir3791 <bir3971@gmail.com>
2023-02-04 17:20:44 +0100

    resolve

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let summa = 0;
+  const sum = (x) => {
+    if (x === undefined) {
+      const result = summa;
+
+      summa = 0;
+
+      return result;
+    }
+
+    summa += x;
+
+    return sum;
+  };
+
+  return sum;
 }
 
 module.exports = makeInfinityAdder;
