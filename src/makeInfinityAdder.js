b011a08216f14a4a35840bfbd81e37a797e3997e
Sasha Green <aleksa146311@gmail.com>
2023-02-13 22:48:22 +0200

    Solution Infinite sum closures

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  const adder = arg => {
+    if (arg === undefined) {
+      const result = amount;
+
+      amount = 0;
+
+      return result;
+    }
+
+    amount += arg;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
