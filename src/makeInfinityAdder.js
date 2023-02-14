c2a1a24913b1f91fe1fce76f8789f7c8b38e3d5a
Vlad Ptashnichenko <vp.vlad.vp@gmail.com>
2023-02-14 20:44:28 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  return function adder(x) {
+    if (!x && x !== 0) {
+      const count = amount;
+
+      amount = 0;
+
+      return count;
+    } else {
+      amount += x;
+
+      return adder;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
