a48eecf01fe312ebb2665acf77283ac9b5ab02d7
timoxa2t <alex2t172@gmail.com>
2023-02-23 19:02:16 +0200

    inception style

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (number) => {
+    if (number === undefined) {
+      const placeholder = result;
+
+      result = 0;
+
+      return placeholder;
+    }
+
+    result += number;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
