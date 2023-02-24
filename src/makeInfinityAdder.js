eed2e9ebc21ceb173f603d1e6ea3c229c1445e4a
Rashyd <rashydlezgi@gmail.com>
2023-02-24 13:11:02 +0200

    infinite adder was created

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const add = (a) => {
+    if (a === undefined) {
+      const final = sum;
+
+      sum = 0;
+
+      return final;
+    }
+
+    sum += a;
+
+    return add;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
