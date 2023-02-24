5a77e8875e018dae67d076b396dbf6d561af5d3f
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-02-24 22:59:12 +0200

    makeInfinityAdder was implement

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(input) {
+    if (!input && input !== 0) {
+      const tempSum = sum;
+
+      sum = 0;
+
+      return tempSum;
+    }
+
+    sum += input;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
