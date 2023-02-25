5790385f7b16f341c4dafa782bb47bba8a15ce22
ctk12 <chetankatwa12@gmail.com>
2023-02-25 10:08:11 +0530

    Solution for Make Infinity Adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,23 @@
 
 function makeInfinityAdder() {
   // write code here
+  let total = 0;
+
+  const add = (value) => {
+    if (value === undefined) {
+      const sum = total;
+
+      total = 0;
+
+      return sum;
+    }
+
+    total += value;
+
+    return add;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
