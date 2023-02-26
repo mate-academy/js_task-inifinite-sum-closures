976f875d2b4c8c71ff0535731be4952ae2efc9b4
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-02-26 20:14:56 +0200

    Infinite Sum Closures - Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,23 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  function adder(number) {
+    if (number !== undefined) {
+      sum += number;
+
+      return adder;
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
