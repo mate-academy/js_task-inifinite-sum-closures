ec384bf924717cdc9125e2bd4224ef8045bf507a
Mark Babich <morak.bobama@gmail.com>
2023-02-27 21:15:49 +0200

    Make Infinity adder task done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let currentSum = 0;
+
+  function adder(number) {
+    if (number === undefined) {
+      const finalSum = currentSum;
+
+      currentSum = 0;
+
+      return finalSum;
+    }
+
+    currentSum += number;
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
