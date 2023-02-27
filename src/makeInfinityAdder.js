6e36665b0edfd3d24cfc224da6491233de8e7578
andrej <andersontwentytwo@gmail.com>
2023-02-27 18:15:03 +0200

    Infinite sum closures

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  const adder = (num = 'nothing passed') => {
+    if (num === 'nothing passed') {
+      const pseudoSum = sum;
+
+      sum = 0;
+
+      return pseudoSum;
+    }
+
+    sum += num;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
