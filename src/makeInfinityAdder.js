79f34be69e22f5d4db1b94c124e902385fc5239a
Alex Nastychenko <nastychenko1993@gmail.com>
2023-02-27 10:24:31 +0200

    fix function args

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -8,15 +8,15 @@
 function makeInfinityAdder() {
   let sum = 0;
 
-  const adder = (...args) => {
-    if (args.length < 1) {
+  const adder = (num) => {
+    if (num === undefined) {
       const result = sum;
 
       sum = 0;
 
       return result;
     } else {
-      sum += args[0];
+      sum += num;
 
       return adder;
     };
