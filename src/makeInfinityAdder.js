c2245bbb85cfcfb556783af39c08928c380c02f7
Oleksandr <98667890az@gmail.com>
2023-03-01 13:54:45 +0200

    Infinity adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,8 +5,22 @@
  * @return {function}
  */
 
-function makeInfinityAdder() {
-  // write code here
+function makeInfinityAdder(num) {
+  let currentSum = 0;
+
+  function toAdd(number) {
+    if (number === undefined) {
+      let sum = currentSum;
+      currentSum = 0;
+
+      return sum;
+    }
+    currentSum += number;
+
+    return toAdd;
+  }
+
+  return toAdd;
 }
 
 module.exports = makeInfinityAdder;
