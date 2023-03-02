3d3b4cfb9a47666d86ff340c1d06ad7acd12a087
Nazar Kharkevych <nkharkevych@gmail.com>
2023-03-02 19:37:43 +0200

    'Solution'

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (...args) => {
+    const returnValue = sum;
+
+    if (!args.length) {
+      sum = 0;
+
+      return returnValue;
+    }
+
+    sum += args.reduce((acc, curr) => acc + curr, 0);
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
