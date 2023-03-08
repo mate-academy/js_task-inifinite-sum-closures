db974040ae0d00754616fbc1658c57516b75e28b
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-03-08 14:34:25 +0300

    'done'

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const makeAdder = () => {
+    let result = 0;
+
+    const adder = function(number) {
+      if (number == null) {
+        const log = result;
+
+        result = 0;
+
+        return log;
+      } else {
+        result += number;
+
+        return adder;
+      }
+    };
+
+    return adder;
+  };
+
+  return makeAdder();
 }
 
 module.exports = makeInfinityAdder;
