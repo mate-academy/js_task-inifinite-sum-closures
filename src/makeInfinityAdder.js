d71c57e705ff41918d9872cf2970e9f2ae1b24de
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-09 20:01:11 +0100

    complete task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let emptyAgain = 0;
+  let sum = 0;
+  const addAllCurry = (a) => {
+    if (a !== undefined) {
+      sum += a;
+
+      return addAllCurry;
+    } else if (emptyAgain > 0) {
+      sum = 0;
+
+      return sum;
+    } else {
+      emptyAgain++;
+
+      return sum;
+    }
+  };
+
+  return addAllCurry;
 }
 
 module.exports = makeInfinityAdder;
