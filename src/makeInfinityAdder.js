ed8f78f07081b70e0550e18e85b547d8b15f540e
EhorSaprykin <astralik1993@gmail.com>
2023-03-10 09:07:50 +0200

    Closure done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,19 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  return function add(x) {
+    if (x || x === 0) {
+      return function(y) {
+        if (y || y === 0) {
+          return add(x + y);
+        } else {
+          return x;
+        };
+      };
+    };
+
+    return 0;
+  };
+};
 
 module.exports = makeInfinityAdder;
