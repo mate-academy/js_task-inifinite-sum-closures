1d138eb1203140dbdb11319427115e3459bcb9b1
Oksana Tytanych <oksanatytanych@gmail.com>
2023-03-13 17:37:56 +0200

    Code was changed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,19 +7,19 @@
 
 function makeInfinityAdder(n) {
   let result = 0;
-  let zm = 0;
+  let sum = 0;
 
   const add = (value) => {
     if (value !== undefined) {
-      zm += value;
+      sum += value;
 
       return add;
-    } else {
-      result = zm;
-      zm = 0;
-
-      return result;
     }
+
+    result = sum;
+    sum = 0;
+
+    return result;
   };
 
   return add;
