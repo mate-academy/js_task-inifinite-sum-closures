b4c47272af352a255163042667aa4312ca1ae06a
Dmytro Somik <dmytro.somik@mccann.com>
2023-03-13 16:27:59 +0000

    Solved

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,26 @@
 
 function makeInfinityAdder() {
   // write code here
+  const arr = [];
+
+  const f = (x) => {
+    if (x === undefined) {
+      const summ = arr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+      );
+
+      arr.length = 0;
+
+      return summ;
+    }
+
+    arr.push(x);
+
+    return f;
+  };
+
+  return f;
 }
 
 module.exports = makeInfinityAdder;
