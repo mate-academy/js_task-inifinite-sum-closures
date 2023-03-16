3dfd0200fb9e049b5c59da32ed594cd66a5a4b15
DmytroHorshunov <cnimipignak@icloud.com>
2023-03-16 19:10:42 +0200

    adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,20 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function adder(num) {
+    if (num === undefined) {
+      const value = result;
+
+      result = 0;
+
+      return value;
+    }
+    result += num;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
