951c6798e31b6d26d4af0e3a4999035c209b1329
Daria <hamdsad8@gmail.com>
2023-03-21 16:17:14 +0200

    created infinity adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,8 +5,42 @@
  * @return {function}
  */
 
+// When the word hell is present in the title of the task,
+// nothing good should be expected. Let's get down to business.
+// Write the makeAdder function, which will return the adder function.
+// The adder function should work on the
+// following pattern: adder(2)(3)(4)(1)(2)(3)(4)() === 19.
+// It will sum all the numbers passed in the parameters until it
+// encounters a call without parameters. When calling without parameters,
+// it will return the result and clear the amount.
+
+// Example:
+
+// const adder = makeAdder();
+// adder() === 0
+// adder(4)(5)() === 9
+// adder() === 0
+// adder(5)(5)(5)
+// adder(4)
+// adder() === 19
+// adder() === 0
+
 function makeInfinityAdder() {
-  // write code here
+  let counter = 0;
+
+  return function adder(num) {
+    if (num === undefined) {
+      const temporaryCounter = counter;
+
+      counter = 0;
+
+      return temporaryCounter;
+    } else {
+      counter += num;
+
+      return adder;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
