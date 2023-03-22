6c3bb74510b72f623185600a9b9557edfc47d28a
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-03-22 08:38:13 +0100

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,66 +6,19 @@
  */
 
 function makeInfinityAdder() {
+  let sum = 0;
+
   const adder = (a) => {
     if (!a && a !== 0) {
-      return 0;
+      const result = sum;
+
+      sum = 0;
+
+      return result;
     }
+    sum += a;
 
-    return (b) => {
-      if (!b && b !== 0) {
-        return a;
-      }
-
-      return (c) => {
-        if (!c && c !== 0) {
-          return a + b;
-        }
-
-        return (d) => {
-          if (!d && d !== 0) {
-            return a + b + c;
-          }
-
-          return (e) => {
-            if (!e && e !== 0) {
-              return a + b + c + d;
-            }
-
-            return (f) => {
-              if (!f && f !== 0) {
-                return a + b + c + d + e;
-              }
-
-              return (g) => {
-                if (!g && g !== 0) {
-                  return a + b + c + d + e + f;
-                }
-
-                return (h) => {
-                  if (!h && h !== 0) {
-                    return a + b + c + d + e + f + g;
-                  }
-
-                  return (i) => {
-                    if (!i && i !== 0) {
-                      return a + b + c + d + e + f + g + h;
-                    }
-
-                    return (j) => {
-                      if (!j && j !== 0) {
-                        return a + b + c + d + e + f + g + h + i;
-                      }
-
-                      return a + b + c + d + e + f + g + h + i + j;
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
-      };
-    };
+    return adder;
   };
 
   return adder;
