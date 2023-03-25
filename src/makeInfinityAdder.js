85d6c6002e100c8def60680c8445048caa1890eb
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-03-25 20:00:04 +0200

    infinity adder was created

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const add = (number) => {
+    if (number) {
+      return (arg) => {
+        if (arg || arg === 0) {
+          return add(number + arg);
+        }
+
+        return number;
+      };
+    }
+
+    return 0;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
