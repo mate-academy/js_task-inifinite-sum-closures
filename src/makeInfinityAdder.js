db1de5d92b02c1f3e312f7c3e4d8a88fb206ffb1
Ch0p3r84 <marin.iliev@yahoo.com>
2023-03-26 17:07:44 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  return function add(...args) {
+    if (args.length === 0) {
+      return 0;
+    }
+
+    let result = args[0];
+
+    return function adder(...newArgs) {
+      if (newArgs.length !== 0) {
+        result += newArgs[0];
+
+        return adder;
+      }
+
+      return result;
+    };
+  };
+};
+
+// const adder = makeInfinityAdder();
+
+// adder(1)(2)(3)(4)(5)(0)(7)(8)(9);
 
 module.exports = makeInfinityAdder;
