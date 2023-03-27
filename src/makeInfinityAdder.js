8f42669fe34a00b9ad58aacd7a14e20e4df21b30
Kyryl <kika_domination@yahoo.com>
2023-03-27 14:12:32 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,33 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let count = 0;
+
+  const adder = num => {
+    if (num === undefined) {
+      count++;
+
+      if (count > 2) {
+        sum = 0;
+        count = 0;
+
+        return sum;
+      }
+
+      return sum;
+    }
+
+    sum += num;
+
+    return adder;
+  };
+
+  if (adder() === undefined) {
+    return 0;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
