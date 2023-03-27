bd4eaa0b9eda95344c50268ebaffe31a35055388
Nataliia Henda <nataliehenda1296@gmail.com>
2023-03-27 11:20:32 +0300

    Make adder function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,26 @@
 
 function makeInfinityAdder() {
   // write code here
+  let summ = 0;
+  let count = 0;
+
+  const checkAdd = (param, returnedFunc) => {
+    if (param || param === 0) {
+      summ += param;
+
+      return returnedFunc;
+    };
+
+    summ = count > 0 ? 0 : summ;
+    count++;
+
+    return summ;
+  };
+
+  const adderFirstFunc = b => checkAdd(b, adderSecondFunc);
+  const adderSecondFunc = a => checkAdd(a, adderFirstFunc);
+
+  return adderFirstFunc;
 }
 
 module.exports = makeInfinityAdder;
