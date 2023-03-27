090b8ed358f914c2a951e4d873f625378214ac44
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-03-27 17:19:58 +0300

    fixed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -13,13 +13,13 @@ function makeInfinityAdder() {
       sum += number;
 
       return adder;
-    } else {
-      const result = sum;
-
-      sum = 0;
-
-      return result;
     }
+
+    const result = sum;
+
+    sum = 0;
+
+    return result;
   };
 }
 
