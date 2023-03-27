ca6e81630d26fff75bf6d1cb2c1ed394cbf90b92
Nazarii Chepil <nazzikk1998@gmail.com>
2023-03-27 19:27:26 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let summ = 0;
+
+  return function addition(number) {
+    if (typeof number === 'number') {
+      summ += number;
+
+      return addition;
+    }
+
+    const resave = summ;
+
+    summ = 0;
+
+    return resave;
+  };
 }
 
 module.exports = makeInfinityAdder;
