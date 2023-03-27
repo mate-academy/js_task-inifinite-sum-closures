c564c1dea332c4f00ad499c0a211db84277faaf8
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-03-27 17:16:00 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(number) {
+    if (typeof number === 'number') {
+      sum += number;
+
+      return adder;
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
