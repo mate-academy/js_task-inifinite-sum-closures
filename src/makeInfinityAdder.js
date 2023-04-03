914a7f981028f4f3eaa5b1a36856a36d5cd0fb99
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-04-03 20:39:31 +0200

    initify adder was made

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,23 @@
 'use strict';
 
-/***
- *
- * @return {function}
- */
-
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (x) => {
+    if (x === undefined) {
+      const OldSum = sum;
+
+      sum = 0;
+
+      return OldSum;
+    }
+
+    sum += x;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
