6f8527e12e57b15fa33ccf6fe07d2d0de562d60f
Natali <swetswet1234@gmail.com>
2023-04-04 22:11:00 +0300

    develop

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,15 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const adder = argument1 => argument2 => {
+    if (argument2 === undefined) {
+      return argument1;
+    }
+
+    return adder(argument1 + argument2);
+  };
+
+  return adder(0);
 }
 
 module.exports = makeInfinityAdder;
