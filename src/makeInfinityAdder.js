aac073e37985e4095b4281a9f72bcb89fc049394
Mikołaj <ostermi@wp.pl>
2023-04-06 14:40:54 +0100

    fix

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -15,11 +15,11 @@ function makeInfinityAdder() {
       sum = 0;
 
       return result;
-    } else {
-      sum += number;
-
-      return adder;
     }
+
+    sum += number;
+
+    return adder;
   }
 
   return adder;
