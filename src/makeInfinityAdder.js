323014b7ee6f9d3d8cb30744bc89663686b20566
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-18 14:17:49 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (number) => {
+    if (number !== undefined) {
+      result += number;
+
+      return adder;
+    } else {
+      const tempResult = result;
+
+      result = 0;
+
+      return tempResult;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
