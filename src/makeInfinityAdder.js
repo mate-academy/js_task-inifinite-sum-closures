141b5f352e002476c51d8630f225775dddb02545
Veronika Pastushkova <d9169393507@gmai.com>
2023-04-20 18:34:32 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (a = null) => {
+    if (a === null) {
+      const newResult = result;
+
+      result = 0;
+
+      return newResult;
+    } else {
+      result += a;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
