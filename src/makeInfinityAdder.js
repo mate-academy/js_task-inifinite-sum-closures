793b66462281cc0be45781b196a077d069e46129
Leonid Krainik <rialleons@gmail.com>
2023-05-05 15:07:32 +0300

    Make infinite adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (...args) => {
+    if (args.length === 0) {
+      const totalSum = sum;
+
+      sum = 0;
+
+      return totalSum;
+    }
+
+    sum += args.reduce((total, elem) => total + elem);
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
