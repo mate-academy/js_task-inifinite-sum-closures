439351ee602a13bcb6314554e51e1a6ea0e8dfb5
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2023-05-08 15:50:27 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,35 @@
 'use strict';
 
 /***
+ * Коли в назві завдання присутне слово пекло, нічого хорошого чукати нше варто.
+  Приступаємо до справи. Напишіть `makeAdder` функцію,
+  яка повертатиме `adder` функцію.
+  Функція`adder` має працювати за шаблоном`adder(2)(3)(4)(1)(2)(3)(4)() === 19`.
+  Він підсомуватиме всі числа, передані в параметрах,
+  доки не зустріне виклик без параметрів.
+  При виклику без параметрів він поверне результат і очистить суму.
  *
  * @return {function}
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(...nums) {
+    if (nums.length === 0) {
+      const resultSum = sum;
+
+      sum = 0;
+
+      return resultSum;
+    }
+
+    sum += nums.reduce((total, elem) => total + elem);
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
