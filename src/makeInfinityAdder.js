a67516600b8a22a57afbf3cab394e7732d3636fb
Mykola Brodyuk <nick.brodyuk@gmail.com>
2023-05-16 11:58:36 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let calculation = 0;
+  let result;
+
+  const sumOfNumbers = nextNumber => {
+    if (nextNumber !== undefined) {
+      calculation += nextNumber;
+    } else {
+      result = calculation;
+      calculation = 0;
+
+      return result;
+    }
+
+    return sumOfNumbers;
+  };
+
+  return sumOfNumbers;
 }
 
 module.exports = makeInfinityAdder;
