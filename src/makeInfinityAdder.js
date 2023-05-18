f271e35536f986a451fc34979aa14e3f8356c920
domkab <dominykas.bobinas@gmail.com>
2023-05-18 13:00:42 +0300

    initial

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,23 @@
 'use strict';
 
-/***
- *
- * @return {function}
- */
-
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const add = number => {
+    if (number === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += number;
+
+    return add;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
