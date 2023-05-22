ab5e652ee7f7b692677ebec9c23a1371b9268b5e
Artem <artem.ivashchenko.exe@gmail.com>
2023-05-22 19:29:17 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let summary = 0;
+
+  function adder(x) {
+    if (x !== undefined) {
+      return (y) => {
+        if (y !== undefined) {
+          summary += x + y;
+
+          return adder(x + y);
+        }
+        summary = 0;
+
+        return x;
+      };
+    }
+
+    return summary;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
