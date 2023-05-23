d7f3acd93c08de4e60d4cd9cdf7d9975080d4bf8
Vania Yarovyi <yarovyi@gmail.com>
2023-05-23 23:39:33 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,28 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let adderResult = 0;
+  let counter = 0;
+
+  return function adder(a, b = adderResult) {
+    counter++;
+
+    if (!a && counter === 1) {
+      adderResult = 0;
+      return adderResult
+    }
+
+    if (a) {
+      adderResult = a + b;
+
+      return adder
+    }
+
+    counter = 0;
+
+    return adderResult;
+  }
 }
 
 module.exports = makeInfinityAdder;
+
