9a3e64d711c482796320aa08e3f4c682e987b072
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-05-23 15:23:10 +0300

    Add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const adder = (a) => {
+    if (a === undefined) {
+      return 0;
+    }
+
+    let sum = a;
+
+    return (b) => {
+      if (b === undefined) {
+        return sum;
+      }
+
+      sum += b;
+
+      return adder(sum);
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
