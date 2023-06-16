0bb0a16455516e4c7ca2ed50de3bfca54ddcb2f1
Eugene Kulish <genekulish13@gmail.com>
2023-06-16 10:43:01 +0300

    solve the task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const addFunction = function(number) {
+    if (number !== undefined) {
+      sum += number;
+
+      return addFunction;
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  };
+
+  return addFunction;
 }
 
 module.exports = makeInfinityAdder;
