097e5fc06ec6b4cc6952f96ab8a8fb1616ff0caa
IlonaBorkovska <borkovska.io@gmail.com>
2023-06-16 13:16:20 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (num1) => {
+    if (num1 === undefined) {
+      const total = sum;
+
+      sum = 0;
+
+      return total;
+    }
+
+    sum += num1;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
