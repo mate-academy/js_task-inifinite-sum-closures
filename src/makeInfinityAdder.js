1213cc2daaa68bd5a8dd514be76865e00f9f2b43
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-06-16 13:58:44 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,8 +5,24 @@
  * @return {function}
  */
 
-function makeInfinityAdder() {
-  // write code here
-}
+const makeInfinityAdder = () => {
+  let sum = 0;
+
+  const adder = num => {
+    if (num === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += num;
+
+    return adder;
+  };
+
+  return adder;
+};
 
 module.exports = makeInfinityAdder;
