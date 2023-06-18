010f0d91d0e70286c06898d4ed230e78b901a861
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-06-18 17:01:25 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (number) => {
+    if (number === undefined) {
+      const temp = sum;
+
+      sum = 0;
+
+      return temp;
+    }
+
+    sum += number;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
