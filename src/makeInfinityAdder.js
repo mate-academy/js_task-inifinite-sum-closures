e2875a7a26a395106b3ba04e209df4fe56b02fbd
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-06-21 19:03:22 +0300

    Add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const infinityAdder = function(num) {
+    if (arguments.length === 0) {
+      const lastSum = sum;
+
+      sum = 0;
+
+      return (lastSum);
+    }
+
+    sum += num;
+
+    return infinityAdder;
+  };
+
+  return infinityAdder;
 }
 
 module.exports = makeInfinityAdder;
