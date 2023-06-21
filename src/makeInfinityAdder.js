95d4da5dc12c186fc76b4c1b2fee7bc2aee99202
Alyona Sarapina <alyona.sarapina@gmail.com>
2023-06-21 18:48:11 +0100

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let arr = [];
+
+  const getValue = (value) => {
+    let num = value;
+
+    while (num !== undefined) {
+      arr.push(num);
+      num++;
+
+      return getValue;
+    }
+
+    const sum = arr.reduce((a, b) => a + b, 0);
+
+    arr = [];
+
+    return sum;
+  };
+
+  return getValue;
 }
 
 module.exports = makeInfinityAdder;
