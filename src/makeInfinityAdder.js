e93c3dc64fb82a67d1bbc398af9050bf092f59a9
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-06-21 14:02:36 +0300

    infinityAdder solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,24 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let emptyCall = 0;
+
+  const adder = (num) => {
+    if (typeof num === 'undefined') {
+      sum = emptyCall > 0
+        ? 0
+        : sum;
+      emptyCall++;
+
+      return sum;
+    }
+    sum += num;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
