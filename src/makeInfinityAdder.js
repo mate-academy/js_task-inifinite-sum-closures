daf712b8e8fc731b8a4a0b591d187c7038fdad74
Kristina Bekher <bekherkr@gmail.com>
2023-06-21 11:55:42 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,33 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (a) => {
+    const sum = (b) => {
+      if (b >= 0) {
+        result += b;
+
+        return sum;
+      }
+
+      const temp = result;
+
+      result = 0;
+
+      return temp;
+    };
+
+    if (a) {
+      result += a;
+
+      return sum;
+    }
+
+    return result;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
