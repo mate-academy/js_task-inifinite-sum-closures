8db49a9fe58354c6989f6cc8df5e9a0d997e3779
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-06-22 13:39:07 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function sum(a) {
+    if (arguments.length === 0) {
+      const lastRes = result;
+
+      result = 0;
+
+      return lastRes;
+    }
+
+    result += a;
+
+    return sum;
+  };
 }
 
 module.exports = makeInfinityAdder;
