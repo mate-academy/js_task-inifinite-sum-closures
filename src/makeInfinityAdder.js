140b133d37f3a057ea571308933ece0d692a9924
zipic <zpc23041988@gmail.com>
2023-06-26 14:18:06 +0300

    infiniteSum

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(...numbers) {
+    if (numbers.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += numbers.reduce((total, number) => total + number, 0);
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
