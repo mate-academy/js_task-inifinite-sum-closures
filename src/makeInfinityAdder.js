5fe70bc92720369298ce420499d87b963080bc68
Nikita Orliak <nikitonorl@gmail.com>
2023-07-09 16:10:12 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,29 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let result = 0;
+
+  const adder = (num) => {
+    if (typeof num !== 'number') {
+      result = 0;
+
+      return result;
+    }
+
+    const add = (number) => {
+      result += num;
+
+      if (typeof number !== 'number') {
+        return result;
+      }
+
+      return adder(number);
+    };
+
+    return add;
+  };
+
+  return adder;
+};
 
 module.exports = makeInfinityAdder;
