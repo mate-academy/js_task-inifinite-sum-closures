9907d5a63bec941e7ec7ddac9a99001b67cdc76e
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-11 21:06:55 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -15,106 +15,14 @@ function makeInfinityAdder() {
       return add;
     } else {
       const result = sum;
+
       sum = 0;
+
       return result;
     }
   }
 
   return add;
-
-  // function adda(a) {
-  //   if (a !== undefined) {
-  //     sum += a;
-  //   } else {
-  //     sum = 0;
-
-  //     return sum;
-  //   }
-
-  //   function addb(b) {
-  //     if (b !== undefined) {
-  //       sum += b;
-  //     } else {
-  //       return sum;
-  //     }
-
-  //     function addc(c) {
-  //       if (c !== undefined) {
-  //         sum += c;
-  //       } else {
-  //         return sum;
-  //       }
-
-  //       function addd(d) {
-  //         if (d !== undefined) {
-  //           sum += d;
-  //         } else {
-  //           return sum;
-  //         }
-
-  //         function adde(e) {
-  //           if (e !== undefined) {
-  //             sum += e;
-  //           } else {
-  //             return sum;
-  //           }
-
-  //           function addf(f) {
-  //             if (f !== undefined) {
-  //               sum += f;
-  //             } else {
-  //               return sum;
-  //             }
-
-  //             function addg(g) {
-  //               if (g !== undefined) {
-  //                 sum += g;
-  //               } else {
-  //                 return sum;
-  //               }
-
-  //               function addh(h) {
-  //                 if (h !== undefined) {
-  //                   sum += h;
-  //                 } else {
-  //                   return sum;
-  //                 }
-
-  //                 function addi(i) {
-  //                   if (i !== undefined) {
-  //                     sum += i;
-  //                   } else {
-  //                     return sum;
-  //                   }
-
-  //                   return sum;
-  //                 }
-
-  //                 return addi;
-  //               }
-
-  //               return addh;
-  //             }
-
-  //             return addg;
-  //           }
-
-  //           return addf;
-  //         }
-
-  //         return adde;
-  //       }
-
-  //       return addd;
-  //     }
-
-  //     return addc;
-  //   }
-
-  //   return addb;
-  // }
-
-  // return adda;
 }
 
 module.exports = makeInfinityAdder;
