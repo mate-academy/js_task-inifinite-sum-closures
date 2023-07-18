f151ccddebf93103e7830a3bf8a6756c5dde9719
Horizon-git <apbreloaded947@gmail.com>
2023-07-18 03:24:06 +0300

    completed task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(num) {
+    if (num === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += num;
+
+      return adder;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
