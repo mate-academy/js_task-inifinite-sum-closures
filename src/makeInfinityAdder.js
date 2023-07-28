15083ff42993197aa738140910a8750d3397b978
Roma <roma150269@gmail.com>
2023-07-28 17:14:02 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function asas(arg) {
+    if (arg !== undefined) {
+      sum += arg;
+
+      return (a) => asas(a);
+    } else {
+      const tmp = sum;
+
+      sum = 0;
+
+      return tmp;
+    }
+  };
+
+  return asas;
 }
 
 module.exports = makeInfinityAdder;
