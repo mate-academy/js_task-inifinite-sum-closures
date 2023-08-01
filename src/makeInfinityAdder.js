828668799a96575038e3ff186c99f9a4af1c4a58
Andrzej Pec <pimatstudio@gmail.com>
2023-08-01 22:28:08 +0200

    soluzione adder dell-infinito

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,25 @@
 'use strict';
 
 /***
- *
  * @return {function}
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(value) {
+    if (value === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += value;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
