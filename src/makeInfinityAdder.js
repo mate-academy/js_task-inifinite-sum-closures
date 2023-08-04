66759eca903e4b8792d146da5f63319c35502692
Anna Shevtsova <aniashevtsova@gmail.com>
2023-08-04 12:02:31 +0200

    all tests pass

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,30 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let amount = 0;
+  const adder = (a) => {
+    if (a === undefined) {
+      const result = amount;
+
+      amount = 0;
+
+      return result;
+    }
+
+    return (b) => {
+      if (b === undefined) {
+        return a;
+      }
+
+      const result = a + b;
+
+      amount += result;
+
+      return adder;
+    };
+  };
+
+  return adder;
+};
 
 module.exports = makeInfinityAdder;
