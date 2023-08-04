6ff922de6e8294faad4f6ab8085c5723cf302de3
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-08-04 12:56:32 +0200

    task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let memory = [];
+
+  const result = function(num) {
+    if (num === undefined) {
+      if (memory.length < 1) {
+        return 0;
+      }
+
+      const tempMemory = memory.reduce((sum, number) => sum + number);
+
+      memory = [];
+
+      return tempMemory;
+    }
+
+    memory.push(num);
+
+    return result;
+  };
+
+  return result;
 }
 
 module.exports = makeInfinityAdder;
