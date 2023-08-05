d7ddd6f35e36edb0f185356627f12720ac3b73d4
Max Hyrych <gggirich@gmail.com>
2023-08-06 00:47:12 +0300

    First commit infinite adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sumParametr = 0;
+
+  const result = function(num) {
+    if (num === undefined) {
+      if (sumParametr === 0) {
+        return 0;
+      }
+
+      const tempSumParametr = sumParametr;
+
+      sumParametr = 0;
+
+      return tempSumParametr;
+    }
+
+    sumParametr += num;
+
+    return result;
+  };
+
+  return result;
 }
 
 module.exports = makeInfinityAdder;
