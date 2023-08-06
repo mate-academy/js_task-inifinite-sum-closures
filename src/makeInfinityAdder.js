1fed8fa66a2c3de8921625e289d5762d96872a03
4xmplme <andreykav2002@gmail.com>
2023-08-07 00:11:30 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(value) {
+    if (arguments.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += value;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
