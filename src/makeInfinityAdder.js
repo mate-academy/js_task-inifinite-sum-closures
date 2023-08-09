8dd6e77b7ddf5b88bb8847aa614032b552efcbd9
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-08-09 18:30:13 +0200

    made infinity adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum;
+  let allNum = [];
+
+  function add(number) {
+    if (number !== undefined) {
+      allNum.push(number);
+
+      return add;
+    } else {
+      sum = allNum.reduce((prev, num) => prev + num, 0);
+      allNum = [];
+
+      return sum;
+    }
+  }
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
