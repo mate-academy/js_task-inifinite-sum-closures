5d4664cb537f0c433f583f964c313a10fb1bb594
NataliMax <maximovs@1876gmail.com>
2023-08-09 11:50:35 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+  let currentResult = 0;
+
+  const adder = (a) => {
+    if (a > -1) {
+      currentResult += a;
+
+      return adder;
+    }
+    result = currentResult;
+    currentResult = 0;
+
+    return result;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
