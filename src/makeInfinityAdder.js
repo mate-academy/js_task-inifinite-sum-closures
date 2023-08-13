47fd68f58bfecbb91d57d165c4c31096cf258fe8
Denys Poltorak <itwfrontend@gmail.com>
2023-08-13 11:35:03 +0300

    feat: add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let collection = 0;
+
+  const adder = (...nums) => {
+    if (!nums.length) {
+      const sum = collection;
+
+      collection = 0;
+
+      return sum;
+    }
+
+    // If the function takes more than one parameter
+    // sum it with reduce
+    collection += nums.reduce((acc, n) => acc + n, 0);
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
