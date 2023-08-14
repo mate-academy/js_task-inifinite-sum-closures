7703f5f91b76e8ca27e9ae211cb3ca1339335545
Nikita <Nehrim24@gmail.com>
2023-08-14 20:14:10 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,26 @@
 
 function makeInfinityAdder() {
   // write code here
+  let SUM = 0;
+  let CURRENT_NUMBERS = [];
+
+  function adder(...args) {
+    if (args.length === 0) {
+      const result = SUM;
+
+      SUM = 0;
+      CURRENT_NUMBERS = [];
+
+      return result;
+    }
+
+    CURRENT_NUMBERS.push(...args);
+    SUM += args.reduce((acc, num) => acc + num, 0);
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
