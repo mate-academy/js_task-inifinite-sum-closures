df1dcd719c3d4283ed3d8dbc41bbedc899a128f8
Kava Bohdan <bogdan.kava.99@gmail.com>
2023-08-14 15:28:44 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function adder(number) {
+    if (arguments.length === 0) {
+      const finalResult = result;
+
+      result = 0;
+
+      return finalResult;
+    }
+
+    result += number;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
