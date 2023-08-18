d435ff197691b07c55332bd0f26cae02c37587d7
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-08-18 10:53:42 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,21 @@
 
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  return function adder(add = false) {
+    sum += add;
+
+    if (add === false) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
