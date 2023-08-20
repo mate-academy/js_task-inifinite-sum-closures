0521423a8c7f7dfc6762c907da5d6684d2a6126e
Valentyn Bilyk <valentynbilyk22@gmail.com>
2023-08-20 10:13:54 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  const adder = function(number) {
+    if (number === undefined) {
+      const result = total;
+
+      total = 0;
+
+      return result;
+    }
+
+    total += number;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
