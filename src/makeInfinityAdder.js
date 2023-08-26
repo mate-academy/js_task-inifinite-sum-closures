7da15fdb93a8e236da53561557d1bba1f2a11c88
Olha Rypich <olha.rypich@gmail.com>
2023-08-26 18:52:43 +0300

    infinity-adder_solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = a => {
+    const result = sum;
+
+    if (a === undefined) {
+      sum = 0;
+
+      return result;
+    }
+
+    sum += a;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
