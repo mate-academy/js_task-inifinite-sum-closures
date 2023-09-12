af5978e66ecb9483845cd5824bfde255698b18b4
Nataliia Kukushkina <n.kukushkina@gmail.com>
2023-09-12 17:43:37 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let addedValues = [];
+
+  const adder = (...numbers) => {
+    if (numbers.length === 0) {
+      const result = addedValues.reduce((sum, item) => sum + item, 0);
+
+      addedValues = [];
+
+      return result;
+    };
+
+    for (const number of numbers) {
+      addedValues.push(number);
+    }
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
