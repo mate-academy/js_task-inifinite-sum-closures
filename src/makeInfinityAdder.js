095581fad55d8451baa70371f59cbb751c6a1df2
Nikita211112 <nikita211112@icloud.com>
2023-09-13 20:47:06 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  function adder(param) {
+    if (param !== undefined) {
+      total += param;
+
+      return adder;
+    } else {
+      const anulation = total;
+
+      total = 0;
+
+      return anulation;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
