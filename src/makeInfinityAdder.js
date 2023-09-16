d940a0e9af0a361625de3462187a09a705e30961
Bahtijar <no4kar@gmail.com>
2023-09-16 11:23:40 +0200

    task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,12 +1,24 @@
 'use strict';
 
-/***
- *
- * @return {function}
- */
-
+/**
+ * @return {function} */
 function makeInfinityAdder() {
-  // write code here
+  const augend = [0, 0];
+
+  const summer = (addend) => {
+    if (addend !== undefined) {
+      augend[0] += addend;
+
+      return summer;
+    }
+
+    augend[1] = augend[0];
+    augend[0] = 0;
+
+    return augend[1];
+  };
+
+  return summer;
 }
 
 module.exports = makeInfinityAdder;
