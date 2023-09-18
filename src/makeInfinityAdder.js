c59da228d06013be93b7ff4110f3675a892e7ec1
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-09-18 18:04:41 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let memory = 0;
+
+  const adder = (number) => {
+    if (number === undefined) {
+      const sum = memory;
+
+      memory = 0;
+
+      return sum;
+    }
+
+    memory += number;
+
+    return (nextNumber) => adder(nextNumber);
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
