5e0c2ea7496f84a59035e3f9cd18cfb86919f353
Denys Vasylenkoe <kapodenim@gmail.com>
2023-09-21 07:13:22 +0300

    v1

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,27 @@
 
 function makeInfinityAdder() {
   // write code here
+  let storedSum = 0;
+
+  function adder(a) {
+    if (a !== undefined) {
+      storedSum += a;
+    } else {
+      storedSum = 0;
+
+      return storedSum;
+    }
+
+    return (b) => {
+      if (b === undefined) {
+        return storedSum;
+      } else {
+        return adder(b);
+      }
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
