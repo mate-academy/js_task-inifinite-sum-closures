b36b9e89410fa080c7a73682983b20fe4b74fa9f
koros <korosov.j@gmail.com>
2023-09-22 11:56:11 +0300

    added task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let prevState;
+
+  const adder = (num) => {
+    if (prevState === undefined) {
+      sum = 0;
+    }
+
+    if (num !== undefined) {
+      sum += num;
+      prevState = num;
+
+      return adder;
+    }
+    prevState = num;
+
+    return sum;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
