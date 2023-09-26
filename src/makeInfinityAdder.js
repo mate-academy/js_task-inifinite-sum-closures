9a7b6e96e82415b5b1b48009b6db529384d2f849
Helen Tsyhypalo <lena.tsigipalo@gmail.com>
2023-09-26 19:55:34 +0200

    add infinity adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (value) => {
+    if (value === undefined) {
+      const sumCopy = sum;
+
+      sum = 0;
+
+      return sumCopy;
+    }
+    sum += value;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
