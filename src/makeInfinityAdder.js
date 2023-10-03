2cf72fe782f0f3dde77128f029007a68413bf4b0
Vika Levik <levika002@gmail.com>
2023-10-03 18:25:00 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  function adder(num) {
+    if (num !== undefined) {
+      result += num;
+
+      return adder;
+    } else {
+      const finalResult = result;
+
+      result = 0;
+
+      return finalResult;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
