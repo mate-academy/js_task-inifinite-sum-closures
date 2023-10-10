161d1b32424ea9e9b5b07b4b70a95971022ff056
Mykhailo Lapchynskyi <mikhaillapchinskyi@gmail.com>
2023-10-10 13:48:25 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,35 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function addder(number) {
+    if (!number && number !== 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    if (number || number === 0) {
+      sum += number;
+
+      return addder;
+    }
+  }
+
+  return addder;
 }
 
 module.exports = makeInfinityAdder;
+
+const adder = makeInfinityAdder();
+
+adder(); // 0;
+adder(4)(5)(); // 9;
+adder(); // 0;
+adder(5)(5)(5);
+adder(4);
+adder(); // 19;
+adder(); // 0;
