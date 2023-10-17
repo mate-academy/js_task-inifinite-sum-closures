4b605279fba2332649690963927b55e091c711b3
Belia Anton <belja.anton@gmail.com>
2023-10-17 22:58:20 +0300

    add function make-infinity-adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let arrayResult = [];
+
+  function adder(number) {
+    if (number === undefined) {
+      const result = arrayResult.length !== 0
+        ? arrayResult.reduce((acc, cur) => acc + cur, 0)
+        : 0;
+
+      arrayResult = [];
+
+      return result;
+    } else {
+      arrayResult.push(number);
+
+      return adder;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
