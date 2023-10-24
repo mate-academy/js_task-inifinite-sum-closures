f16a1c1e43ee775ec27fc497e59bc2c94975b1c5
Volodya Mursky <volodyamurskyi@gmail.com>
2023-10-24 14:21:00 +0300

    solution-v

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let total = 0;
+  const adder = (a) => {
+    if (a || a === 0) {
+      total += a;
 
+      return adder;
+    } else if (!a) {
+      const result = total;
+
+      total = 0;
+
+      return result;
+    }
+  };
+
+  return adder;
+}
 module.exports = makeInfinityAdder;
