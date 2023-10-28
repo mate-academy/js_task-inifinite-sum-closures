2934c982dfe3898783e16ea6be2c9596e5e1188e
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2023-10-28 23:48:20 +0300

    Add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  const adder = function(num) {
+    if (typeof num === 'number') {
+      amount += num;
+
+      return adder;
+    }
+
+    const result = amount;
+
+    amount = 0;
+
+    return result;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
