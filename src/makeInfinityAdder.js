ab15049d222112fa1d5a5afb661c8ae8378b078c
Maryna Shmyhelska <shmigelska.m.y@gmail.com>
2023-10-31 13:11:55 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const makeSum = (a) => {
+    if (a === undefined) {
+      return 0;
+    }
+
+    return (b) => {
+      if (b === undefined) {
+        return a;
+      } else {
+        return makeSum(a + b);
+      }
+    };
+  };
+
+  return makeSum;
 }
 
 module.exports = makeInfinityAdder;
