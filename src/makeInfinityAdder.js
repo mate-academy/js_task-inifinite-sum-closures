951053c76cbb61ce82b2b41bc07a736208928b2d
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2023-11-05 19:37:10 +0200

    first add

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let totalSum = 0;
+
+  return function add(a) {
+    const compSum = totalSum;
+    const numberIn = a;
+
+    if (numberIn || numberIn === 0) {
+      totalSum += numberIn;
+
+      return add;
+    } else {
+      totalSum = 0;
+
+      return compSum;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
