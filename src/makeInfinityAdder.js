c8a8a9300bcee91985fbdb2412bd4bf606a92815
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2023-11-09 17:01:32 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let currentSum = 0;
+  const adder = (n) => {
+    if (n !== undefined) {
+      currentSum += n;
+
+      return adder;
+    }
+
+    const total = currentSum;
+
+    currentSum = 0;
+
+    return total;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
