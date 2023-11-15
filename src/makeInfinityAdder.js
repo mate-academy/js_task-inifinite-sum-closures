38ef2b92f7dd7e5c156330428229616526566f63
boytrend0108 <boytrend@gmail.com>
2023-11-15 18:28:47 +0200

    complete task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,33 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (a) => {
+    if (a === undefined) {
+      const res = sum;
+
+      sum = 0;
+
+      return res;
+    }
+
+    sum += a;
+
+    return (b) => {
+      if (b === undefined) {
+        const res = sum;
+
+        sum = 0;
+
+        return res;
+      }
+
+      return adder(b);
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
