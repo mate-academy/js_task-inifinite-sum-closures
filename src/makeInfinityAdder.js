35b07f9db4c8676211d59f83b7332b1cc9bad2d8
Fedos17 <dfkd81@gmail.com>
2023-11-19 16:16:39 +0200

    result

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let countResult = 0;
+
+  const adder = (a) => {
+    if (!isNaN(a)) {
+      countResult += a;
+    } else {
+      return 0;
+    }
+
+    return function addsNext(b) {
+      if (!isNaN(b)) {
+        countResult += b;
+
+        return addsNext;
+      } else {
+        return countResult;
+      }
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
