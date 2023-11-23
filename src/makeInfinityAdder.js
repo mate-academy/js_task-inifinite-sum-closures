61285cefe714d7fd74d144d91e5526f2107d9bec
Oleksii Bielovolov <whiteoxofficial@gmail.com>
2023-11-23 17:37:19 +0200

    infiniteSum

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,34 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  // first solution
+
+  // function adder(a) {
+  //   if (typeof a === 'undefined') {
+  //     return 0;
+  //   }
+
+  //   function makeAdder(b) {
+  //     if (typeof b !== 'undefined') {
+  //       return adder(a + b);
+  //     } else {
+  //       return a;
+  //     }
+  //   }
+
+  //   return makeAdder;
+  // }
+
+  // return adder;
+
+  // short solution
+  const adder = a => typeof a !== 'undefined'
+    ? b => typeof b !== 'undefined'
+      ? adder(a + b)
+      : a
+    : 0;
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
