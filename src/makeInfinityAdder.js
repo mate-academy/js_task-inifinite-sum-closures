cc61ffa15d68e56cf5d307e941399b04b0f21803
Dmytro Kretov <dmytro.kretov@gmail.com>
2023-11-23 14:20:07 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  function adder(a) {
+    if (a) {
+      result += a;
+    }
+
+    if (a === undefined) {
+      const lastResult = result;
+
+      result = 0;
+
+      return lastResult;
+    }
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
