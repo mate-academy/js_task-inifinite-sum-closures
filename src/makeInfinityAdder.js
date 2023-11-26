c68d231108029e0964984c7616d69d9ecd3cb3d6
foxstudent103376 <yashopua@gmail.com>
2023-11-26 20:13:05 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let counter = 0;
+
+  const adder = function(arg) {
+    if (arg || arg === 0) {
+      counter += arg;
+
+      return adder;
+    } else {
+      const ans = counter;
+
+      counter = 0;
+
+      return ans;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
