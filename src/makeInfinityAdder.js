d33200022d333c358a779d88469d6e845c479343
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-11-27 23:33:23 +0200

    task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  
+  let sum = 0;
+
+  const adder = (value) => {
+    if (value === undefined) {
+      const returnedValue = sum;
+
+      sum = 0;
+
+      return returnedValue;
+    } else {
+      sum += value;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
