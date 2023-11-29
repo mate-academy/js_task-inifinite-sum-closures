6d9635add2e0493a25a765a5088bc643c27e733f
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2023-11-29 02:12:16 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = [];
+
+  function adder(value) {
+    if (value === undefined) {
+      const summa = result.reduce((total, num) => total + num, 0);
+
+      result = [];
+
+      return summa;
+    }
+
+    result.push(value);
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
