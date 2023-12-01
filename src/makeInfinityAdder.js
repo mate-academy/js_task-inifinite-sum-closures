97ec49b47c644c84f521e570661acea60426c177
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2023-12-01 22:02:05 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let storedResult = 0;
+
+  function adder(a) {
+    if (typeof a === 'number') {
+      storedResult += a;
+
+      return adder;
+    }
+
+    const result = storedResult;
+
+    storedResult = 0;
+
+    return result;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
