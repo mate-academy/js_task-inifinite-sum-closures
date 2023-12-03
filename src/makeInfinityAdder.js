e5e30ecc512a1352bd4294b139e44bf60d3804c0
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2023-12-03 19:39:34 +0100

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let callCount = 0;
+
+  const adder = function(a) {
+    if (a !== undefined) {
+      callCount += a;
+
+      return adder;
+    } else {
+      const result = callCount;
+
+      callCount = 0;
+
+      return result;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
