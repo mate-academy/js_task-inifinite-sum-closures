edd943273a3d0ea294875d39c431de9b9232daca
Volodymyr Baraban <barabanv2003@gmail.com>
2023-12-03 21:06:34 +0200

    makeInfinityAdded

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(...args) {
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += args.reduce((acc, val) => acc + val, 0);
+
+      return adder;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
