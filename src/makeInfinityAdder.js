55ff1071b82fee494af5ba5643dbdb7f377442a9
unknown <anorifrell@gmail.com>
2023-12-05 15:54:29 +0200

    add infinite sum closures

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,23 @@
 
 function makeInfinityAdder() {
   // write code here
+  let result = 0;
+
+  const adder = (a = null) => {
+    if (a == null) {
+      const last = result;
+
+      result = 0;
+
+      return last;
+    } else {
+      result += a;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
