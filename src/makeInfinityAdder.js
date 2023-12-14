b1794a2ab9b804adf06a41f75bdc432a674acf10
Nazar Malakhovskiy <malahovskinazar007@gmail.com>
2023-12-14 22:59:29 +0200

    Solution by GTXtab

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,26 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let numbers = [];
+
+  function adder(num) {
+    if (num !== undefined) {
+      sum += num;
+      numbers.push(sum);
+
+      return adder;
+    } else {
+      const result = sum;
+
+      sum = 0;
+      numbers = [];
+
+      return result;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
