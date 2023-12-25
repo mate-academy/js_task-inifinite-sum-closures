5413e54bea33cf195e9e75fa7e6e19bb01de9b3b
Rostyslav Tkachuk <pro100kor.moon@gmail.com>
2023-12-25 18:46:55 +0200

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  function f(b) {
+    if (b !== undefined) {
+      amount += b;
+    } else {
+      const result = amount;
+
+      amount = 0;
+
+      return result;
+    }
+
+    return f;
+  }
+
+  return f;
 }
 
 module.exports = makeInfinityAdder;
