0457cf31d6101044dc9190b9da632ae0562cbcc2
Dmytro Moshkivskyi <dmosh@ukr.net>
2023-12-25 16:29:09 +0200

    js_task-inifinite-sum-closures by Dimoshua

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (a) => {
+    if (a !== undefined) {
+      sum = sum + a;
+
+      return adder;
+    } else {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
