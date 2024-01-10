199b2827169353b9129be074f882d3fcaa3ea113
MrWhiteres <alexandr.leluk.99@gmail.com>
2024-01-10 22:08:36 +0200

    add task solutions

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,8 +5,24 @@
  * @return {function}
  */
 
-function makeInfinityAdder() {
-  // write code here
-}
+const makeInfinityAdder = () => {
+  let sum = 0;
+
+  const adder = (value) => {
+    if (value === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += value;
+
+    return adder;
+  };
+
+  return adder;
+};
 
 module.exports = makeInfinityAdder;
