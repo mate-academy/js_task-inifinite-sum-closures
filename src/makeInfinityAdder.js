31c4511d620086485a7b04a2ced9612f1d380f5e
Дядя Саня <alvlru1971@gmail.com>
2024-01-11 12:53:31 +0200

    my_decision

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let plus = 0;
+
+  const adder = (...args) => {
+    if (args.length === 0) {
+      const result = plus;
+      plus = 0;
+
+      return result;
+    } else {
+      plus += args.reduce((prev, current) => prev + current, 0);
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
