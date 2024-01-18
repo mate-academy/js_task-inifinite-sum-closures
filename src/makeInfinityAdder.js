00402526fcdc3c48c93745545e2bd012ef01c125
Nikita <2004nnikita@gmail.com>
2024-01-19 00:15:25 +0100

    second

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -9,16 +9,16 @@ function makeInfinityAdder() {
   let sum = 0;
 
   function makeAdder(num) {
-    sum += num;
-
     if (num === undefined) {
         const result = sum;
         sum = 0;
         return result;
     }
+    sum += num;
     return makeAdder;
   }
 
   return makeAdder;
 }
+
 module.exports = makeInfinityAdder;
