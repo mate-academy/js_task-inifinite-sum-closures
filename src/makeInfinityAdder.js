42f2ba441b4dd43744ea31233c78325d597158d6
“Julia <j.shkorupatska@gmail.com>
2024-01-21 15:06:32 +0200

    developed makeInfinityAdder function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (number) => {
+    const currSum = sum;
+
+    if (number === undefined) {
+      sum = 0;
+
+      return currSum;
+    }
+
+    sum += number;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
