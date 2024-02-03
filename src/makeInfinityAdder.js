966c414525bb39e85ef8a5dd14242ee089b177e9
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-02-03 20:05:50 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const add = function(num) {
+    if (num !== undefined) {
+      sum += num;
+
+      return add;
+    }
+
+    const result = sum;
+
+    sum = 0;
+
+    return result;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
