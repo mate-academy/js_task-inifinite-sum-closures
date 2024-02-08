6d5681aa779b538e417cf8eaa1bb89d1a58e6ec1
Богдан <BoGoBooM@MacBook-Pro---Bohdan.local>
2024-02-08 19:02:22 +0200

    task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  function add(value) {
+    if (value === undefined) {
+      const currentSum = result;
+
+      result = 0;
+
+      return currentSum;
+    }
+
+    result += value;
+
+    return add;
+  }
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
