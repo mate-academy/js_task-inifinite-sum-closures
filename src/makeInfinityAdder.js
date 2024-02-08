428841bc0963df1cb1d08345981af5fec4e43092
Olha Shvets <lv160888soi@gmail.com>
2024-02-08 20:37:04 +0200

    add a solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,29 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  const adder = (num) => {
+    let sum = 0;
+
+    if (num === undefined) {
+      return sum;
+    }
+
+    sum += num;
+
+    const innerAdder = (nextNum) => {
+      if (nextNum === undefined) {
+        return sum;
+      }
+
+      sum += nextNum;
+
+      return innerAdder;
+    };
+
+    return innerAdder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
