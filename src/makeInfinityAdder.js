8d80cc3205a2eaec43625ed54e824e4602534a14
Nikita Ganzha <geekneek28@gmail.com>
2024-02-14 15:03:45 +0200

    added solution infinite sum

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,21 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let totalSum = 0;
+
+  return function adder(...args) {
+    if (args.length === 0) {
+      const returnTotalSum = totalSum;
+
+      totalSum = 0;
+
+      return returnTotalSum;
+    }
+
+    totalSum += Number(args);
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
