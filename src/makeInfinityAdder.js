228547263a0ad59478c55f7cef8dd9bc97016ad5
Valentyna Lytvynenko <valentina.vik10@gmail.com>
2024-02-19 20:18:47 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,8 +5,24 @@
  * @return {function}
  */
 
-function makeInfinityAdder() {
-  // write code here
+function makeInfinityAdder(arg) {
+  let sum = 0;
+
+  function adder(newArg) {
+    if (newArg === undefined) {
+      const curentSum = sum;
+
+      sum = 0;
+
+      return curentSum;
+    }
+
+    sum += newArg;
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
