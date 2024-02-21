ea362b1832f14b8f312d42c4a4711eca549600a8
beliyandr <beliyandr@gmail.com>
2024-02-21 19:59:40 +0200

    firts variant

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const result = (number) => {
+    if (number || number >= 0 || number <= 0) {
+      sum += number;
+
+      return result;
+    }
+
+    if (number === undefined) {
+      const res = sum;
+
+      sum = 0;
+
+      return res;
+    }
+  };
+
+  return result;
 }
 
 module.exports = makeInfinityAdder;
