078e2ed7f99b2b62c55c5db2f73992db6447a2d1
Daruna Kolinska <daruna.kolinska@gmail.com>
2024-02-22 20:33:05 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,22 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(...nums) {
+    if (nums.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+    sum += nums.reduce((acc, num) => acc + num, 0);
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
