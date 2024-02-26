83bd688c35f456a3749e11a4762adc15b741496f
Oleh Duma <hakerprof@gmail.com>
2024-02-26 17:11:41 +0200

    improve solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,18 +7,19 @@
 
 function makeInfinityAdder() {
   let result = 0;
-  const adder = (...args) => {
-    if (args.length === 0) {
+
+  const adder = (number) => {
+    if (number !== undefined) {
+      result += number;
+
+      return adder;
+    } else {
       const currentResult = result;
 
       result = 0;
 
       return currentResult;
     }
-
-    result += args.reduce((acc, val) => acc + val, 0);
-
-    return adder;
   };
 
   return adder;
