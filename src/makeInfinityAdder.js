9efe67b2712729b38e5bb85b4a197f8b23202f03
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-02-26 18:58:01 +0200

    some optimization

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,24 +4,16 @@
  *
  * @return {function}
  */
-
 function makeInfinityAdder() {
-  let clearCall = 0;
   let result = 0;
 
   const adder = (arg) => {
     if (arg === undefined) {
-      clearCall++;
+      const currentResult = result;
 
-      if (clearCall === 1) {
-        return result;
-      }
+      result = 0;
 
-      if (clearCall === 2) {
-        result = 0;
-
-        return result;
-      }
+      return currentResult;
     }
 
     result += arg;
