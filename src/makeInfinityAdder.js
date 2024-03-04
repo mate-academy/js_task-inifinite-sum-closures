3fdd554edea86338b69456048d81c70bf33c9447
Oleh Marushchak <westernpc22@gmail.com>
2024-03-04 11:50:19 +0200

    add test

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let sum = [];
+
+  const adder = (a) => {
+    if (a || a === 0) {
+      sum.push(a);
+    } else {
+      const copySum = [...sum];
+
+      sum = [];
+
+      return copySum.reduce((prev, b) => prev + b, 0);
+    }
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
