5cddf9f10e8a55d3d1066ed7e937625f0602a51f
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-03-04 17:00:51 +0200

    implemented the requirements

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,27 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (number) => {
+    return createFunction(number);
+  };
+
+  const createFunction = (number) => {
+    if (number || number === 0) {
+      result += number;
+
+      return createFunction;
+    } else {
+      const currentResult = result;
+
+      result = 0;
+
+      return currentResult;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
