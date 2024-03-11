6b60f30e82c3234369e0a24a4a5a5ca5c9f3ce3f
rwlp <rafaelwesleylima@gmail.com>
2024-03-11 13:13:36 -0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,9 +4,25 @@
  *
  * @return {function}
  */
-
 function makeInfinityAdder() {
   // write code here
+  let b = 0;
+
+  const sum = function (a) {
+    if (a !== undefined) {
+      b += a;
+
+      return sum;
+    }
+
+    const c = b;
+
+    b = 0;
+
+    return c;
+  };
+
+  return sum;
 }
 
 module.exports = makeInfinityAdder;
