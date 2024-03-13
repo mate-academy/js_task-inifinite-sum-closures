947485da23b63621d28833e4955e1047a72ec095
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-03-13 18:44:12 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,24 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
-}
+  let total = 0;
 
+  function adder(...nums) {
+    if (!nums.length) {
+      const result = total;
+
+      total = 0;
+
+      return result;
+    }
+
+    for (const num of nums) {
+      total += num;
+    }
+
+    return adder;
+  }
+
+  return adder;
+}
 module.exports = makeInfinityAdder;
