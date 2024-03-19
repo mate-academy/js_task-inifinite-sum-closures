99e30902bd0f869b4f9537f384d8ac1a1fe5f0e1
ihor_romaniuk <iromaniuk@mycubes.nl>
2024-03-19 16:59:24 +0200

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,10 +1,27 @@
-'use strict';
+('use strict');
 
 /***
  * @return {function}
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  const func = (param) => {
+    if (param || param === 0) {
+      sum += param;
+
+      return func;
+    } else {
+      const res = sum;
+
+      sum = 0;
+
+      return res;
+    }
+  };
+
+  return func;
 }
 
 module.exports = makeInfinityAdder;
