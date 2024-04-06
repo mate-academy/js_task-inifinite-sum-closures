a0afcf2d1426995b21bc8cb90d1da26191675ee5
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-04-06 20:09:32 +0300

    task completed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let finalValue = 0;
+
+  const adder = (n) => {
+    if (n === undefined) {
+      finalValue = sum;
+      sum = 0;
+
+      return finalValue;
+    }
+
+    sum += n;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
