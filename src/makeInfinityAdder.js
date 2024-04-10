74fb09b2215beeaaa33c5511a4d1bc38ff153a71
mate-student <166617557+mate-student@users.noreply.github.com>
2024-04-10 18:52:51 +0300

    Update makeInfinityAdder.js

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  const adder = (a) => {
+    if (a !== undefined) {
+      amount += a;
+
+      return adder;
+    } else {
+      const currentAmount = amount;
+
+      amount = 0;
+
+      return currentAmount;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
