51e34161a1b5c35b5fa393bba638d6471021c9a7
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-11 19:20:34 +0300

    solution 1

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,10 +1,35 @@
 'use strict';
 
 /***
+ *
  * @return {function}
  */
+
 function makeInfinityAdder() {
-  // write code here
+  let clearCall = 0;
+  let result = 0;
+
+  const adder = (arg) => {
+    if (arg === undefined) {
+      clearCall++;
+
+      if (clearCall === 1) {
+        return result;
+      }
+
+      if (clearCall === 2) {
+        result = 0;
+
+        return result;
+      }
+    }
+
+    result += arg;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
