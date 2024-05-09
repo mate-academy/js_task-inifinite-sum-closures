bcd9e649eb6ff57597b663d3b3af8a496a813fef
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-05-09 19:56:09 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,8 +3,14 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
-  // write code here
+function makeInfinityAdder(a = 0) {
+  return (b) => {
+    if (typeof b !== 'number') {
+      return a;
+    }
+
+    return makeInfinityAdder(a + b);
+  };
 }
 
 module.exports = makeInfinityAdder;
