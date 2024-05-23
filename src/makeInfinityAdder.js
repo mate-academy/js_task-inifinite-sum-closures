194072a101dca86f58ca431618f9482bf10dd706
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-05-23 14:32:09 +0300

    Inifinite-sum-closures

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,14 +4,12 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  let totalSum = 0;
   let currentSum = 0;
 
   function adder(num) {
     if (num === undefined) {
-      const result = totalSum + currentSum;
+      const result = currentSum;
 
-      totalSum = 0;
       currentSum = 0;
 
       return result;
@@ -21,13 +19,6 @@ function makeInfinityAdder() {
       return adder;
     }
   }
-
-  adder.toString = () => {
-    totalSum += currentSum;
-    currentSum = 0;
-
-    return totalSum;
-  };
 
   return adder;
 }
