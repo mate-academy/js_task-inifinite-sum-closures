8840f42811fa69466133f81497590cdf5ca9b343
Kryvytskyi Volodymyr <v.kryvytskyy@mate.academy>
2024-06-03 17:10:31 +0300

    omni solution 3

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,24 +5,31 @@
  */
 function makeInfinityAdder() {
   let totalSum = 0;
+  let currentSum = 0;
 
-  function add(num) {
-    if (num !== undefined) {
-      totalSum += num;
-
-      return add;
-    }
-
+  function adder(num) {
     if (num === undefined) {
-      const newSum = totalSum;
+      const result = totalSum + currentSum;
 
       totalSum = 0;
+      currentSum = 0;
 
-      return newSum;
+      return result;
+    } else {
+      currentSum += num;
+
+      return adder;
     }
   }
 
-  return add;
+  adder.toString = () => {
+    totalSum += currentSum;
+    currentSum = 0;
+
+    return totalSum;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
