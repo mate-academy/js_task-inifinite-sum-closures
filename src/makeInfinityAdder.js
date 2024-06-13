2f99a1fccbabb450973b2a9271216e49dea9331b
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-13 14:26:55 +0300

    [Feature] created makeAdder function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,27 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let result = 0;
+
+  const adder = (a) => {
+    if (typeof a !== 'number') {
+      if (!result) {
+        result = sum;
+      } else {
+        sum = 0;
+        result = 0;
+      }
+
+      return result;
+    }
+
+    sum += a;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
