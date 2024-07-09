c44f88fe6fbb3871efb5af8c12fed8f4529a49a5
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-07-09 19:35:13 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  const adder = (...num) => {
+    if (num.length === 0) {
+      const tmp = sum;
+
+      sum = 0;
+
+      return tmp;
+    }
+
+    sum = num.reduce((el1, el2) => el1 + el2, sum);
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
