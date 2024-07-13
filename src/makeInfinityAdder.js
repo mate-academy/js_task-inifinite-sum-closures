e3a3ebb4c25556e05baf8ec0132b3b9078c6e5b5
SerhiiSemennikov <semennik@hotmail.com>
2024-07-13 12:03:55 +0200

    new

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+  let result = 0;
+
+  function adder(a) {
+    if (a === undefined) {
+      result = sum;
+      sum = 0;
+
+      return result;
+    }
+
+    sum += a;
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
