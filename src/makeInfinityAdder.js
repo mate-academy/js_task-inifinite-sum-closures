a300e292465ee96322ff7717ceacdbf401bb1f41
Teollan <serhiikofficial@gmail.com>
2024-07-15 13:31:47 +0300

    added type check

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,6 +7,10 @@ function makeInfinityAdder() {
   let sum = 0;
 
   const adder = (value) => {
+    if (typeof value !== 'number' && typeof value !== 'undefined') {
+      throw new Error(`Argument can only be of type 'number' or 'undefined'`);
+    }
+
     if (value === undefined) {
       const result = sum;
 
