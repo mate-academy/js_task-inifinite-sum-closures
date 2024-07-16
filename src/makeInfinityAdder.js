7ba83f0c8e9332832914de4b80a29fe527e92aea
Kirill Gasan <dp160190gkj@gmail.com>
2024-07-16 18:52:16 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,31 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  function adder(data) {
+    if (arguments.length > 1) {
+      return 'Error, data isn`t single number!';
+    }
+
+    if (data === undefined) {
+      const tempTotal = total;
+
+      total = 0;
+
+      return tempTotal;
+    }
+
+    if (!Number(data) && data !== 0) {
+      return 'Error, data isn`t number!';
+    } else {
+      total += data;
+
+      return adder;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
