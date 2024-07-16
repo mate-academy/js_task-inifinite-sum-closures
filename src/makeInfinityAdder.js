517c11cf8ac4c8833c269d25723a845f0f6ecb39
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-07-16 12:41:26 +0200

    Solution1

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,17 +6,25 @@
 function makeInfinityAdder() {
   let sum = 0;
 
-  function adder(number) {
-    if (number === undefined) {
+  function adder(item) {
+    if (arguments.length > 1) {
+      return 'Error';
+    }
+
+    if (item === undefined) {
       const result = sum;
 
       sum = 0;
 
       return result;
     } else {
-      sum += number;
+      if (!Number(item) && item !== 0) {
+        return 'Error';
+      } else {
+        sum += item;
 
-      return adder;
+        return adder;
+      }
     }
   }
 
