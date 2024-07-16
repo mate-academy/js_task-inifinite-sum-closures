f5460f9151c046e74bcff217c40f3c36170b9c7e
Your Name <ruslankurman13@gmail.com>
2024-07-16 13:50:58 +0300

    task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,42 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  function adder(value) {
+    let sum = value;
+
+    function innerAdder(nextValue) {
+      if (arguments.length === 0) {
+        return sum;
+      }
+      sum += nextValue;
+
+      return innerAdder;
+    }
+
+    return innerAdder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
+
+// function makeInfinityAdder() {
+//   function adder(value) {
+//     let sum = value;
+//     function innerAdder(nextValue) {
+//       if (arguments.length === 0) {
+//         return sum;
+//       }
+//       sum += nextValue;
+
+//       console.log(sum);
+//       return innerAdder;
+//     }
+//     return innerAdder;
+//   }
+
+//   return adder;
+// }
+
+// window.makeInfinityAdder = makeInfinityAdder;
