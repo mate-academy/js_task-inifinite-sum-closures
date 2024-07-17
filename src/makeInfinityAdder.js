beb0fb7881b6227a175b9242b8276fde73745ac9
Valentyn Radobenko <valikradobenko@gmail.com>
2024-07-17 17:29:42 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,31 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  const adder = function (a) {
+    let sum;
+
+    if (a === undefined) {
+      if (sum === undefined) {
+        sum = 0;
+      }
+
+      return sum;
+    }
+
+    sum = a;
+
+    return (b) => {
+      if (b === undefined) {
+        return sum;
+      }
+
+      sum += b;
+
+      return adder(sum);
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
