048592f6d7bd728876211cd83d7e29b99f4b8159
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-07-19 02:03:06 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const call = (...args) => {
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += args[0];
+
+      return call;
+    }
+  };
+
+  return call;
 }
 
 module.exports = makeInfinityAdder;
