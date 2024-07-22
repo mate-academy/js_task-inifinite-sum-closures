cc8905fdf32093b162cfa325b0f232d18b82a20b
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2024-07-22 23:13:36 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,25 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  function adder(...nums) {
+    if (nums.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      for (const num of nums) {
+        sum += num;
+      }
+
+      return adder;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
