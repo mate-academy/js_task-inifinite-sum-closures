d89c37fc7e4ce48530512f231a05e00d1342f86d
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-07-22 02:31:52 +0200

    fix

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,15 +6,15 @@
 function makeInfinityAdder() {
   let sum = 0;
 
-  const call = (...args) => {
-    if (args.length === 0) {
+  const call = (arg) => {
+    if (arg === undefined) {
       const result = sum;
 
       sum = 0;
 
       return result;
     } else {
-      sum += args[0];
+      sum += arg;
 
       return call;
     }
