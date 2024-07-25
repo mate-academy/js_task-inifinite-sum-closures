1f819bb34921730140efaf4d64b2648a39249354
IURII FEDYNA <iurii.fedyna@gmail.com>
2024-07-25 09:57:23 -0230

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  function adder(...args) {
+    if (args.length === 0) {
+      const result = total;
+
+      total = 0;
+
+      return result;
+    } else {
+      total += args.reduce((sum, num) => sum + num, 0);
+
+      return adder;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
