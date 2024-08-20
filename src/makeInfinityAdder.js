61e1552af2abb400c794d7812608c2f383b6cdf7
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-08-20 08:43:41 +0300

    add task soultion

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,26 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let totalSum = 0;
+
+  const adderNum = (a) => {
+    if (a === undefined) {
+      let result = 0;
+
+      if (totalSum > 0) {
+        result = totalSum;
+        totalSum = 0;
+      }
+
+      return result;
+    }
+
+    totalSum += a;
+
+    return adderNum;
+  };
+
+  return adderNum;
 }
 
 module.exports = makeInfinityAdder;
