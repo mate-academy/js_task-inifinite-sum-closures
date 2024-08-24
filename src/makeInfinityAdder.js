e768f3f72176d331145abd69fec8265b45b8609a
Ivan Vaniuk <ivanvanuk77@gmail.com>
2024-08-24 13:57:29 +0300

    Done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function add(...args) {
+    if (args.length === 0) {
+      const res = sum;
+
+      sum = 0;
+
+      return res;
+    }
+
+    sum += args.reduce((acc, val) => acc + val, 0);
+
+    return add;
+  }
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
