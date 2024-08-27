b4eb24bd7f60a749234e3e863d7590a04580c6b1
Yevhen Stepanenko <yevhen.s@mate.academy>
2024-08-27 11:43:35 +0300

    first commit

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const makeAdder = (number) => {
+    if (number !== undefined) {
+      sum += number;
+
+      return makeAdder;
+    }
+
+    const lastValue = sum;
+
+    sum = 0;
+
+    return lastValue;
+  };
+
+  return makeAdder;
 }
 
 module.exports = makeInfinityAdder;
