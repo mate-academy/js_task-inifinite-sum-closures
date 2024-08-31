1aebb72e89d3127649f8e6fef318d0665476d1cb
Vika <rudaviktoria0@gmail.com>
2024-08-31 11:23:57 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,24 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  // eslint-disable-next-line no-unused-vars
+  let sum = 0;
+
+  const sumNumbers = (arg) => {
+    if (arg === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += arg;
+
+    return sumNumbers;
+  };
+
+  return sumNumbers;
 }
 
 module.exports = makeInfinityAdder;
