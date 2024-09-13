15c7c2f4520cd1ebc64bccb9730997225d61357d
Anna <annanicky90gmail.com>
2024-09-13 13:20:00 +0400

    create `adder` function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  const adder = (arg) => {
+    if (arg === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+    sum += arg;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
