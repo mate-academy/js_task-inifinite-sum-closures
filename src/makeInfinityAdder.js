d7b9705cc5446c81df528bb56499171082ad13ed
Yevhen Rudnytskyi <yevhen.dodev@gmail.com>
2024-09-17 20:20:09 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0; // Initialize the sum to zero
+
+  function adder(...args) {
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0; // Reset the sum after returning the result
+
+      return result;
+    }
+    // Sum up all the arguments passed
+    sum += args.reduce((acc, num) => acc + num, 0);
+
+    return adder; // Return the adder function to allow chaining
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
