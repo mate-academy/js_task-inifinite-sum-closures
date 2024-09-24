7d80a4241021a4757edd451d3e3482196dc1ab8c
an-marryKyslenko <olih.a@icloud.com>
2024-09-24 13:01:14 +0200

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,10 +1,38 @@
 'use strict';
 
+const { run } = require("jest");
+
 /***
  * @return {function}
  */
 function makeInfinityAdder() {
   // write code here
+  const adder = (a) => {
+    let count = 0;
+    let total = a;
+
+    if(count === 0 && a === undefined){
+      return 0;
+    }
+    count++;
+    const inner = (b) => {
+      if (b === undefined) {
+        const saveTotal = total;
+        count = 0;
+        total = 0;
+        return saveTotal;
+      }
+      total += b;
+      return inner;
+    }
+
+    if(a !== undefined) {
+      return inner;
+    }
+
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
