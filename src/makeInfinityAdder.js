33bc599be44d343fcecf76de99c1e7a8f97762d8
Pasha Shchukin <pasha4320692@gmail.com>
2024-09-29 12:25:05 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -7,18 +7,18 @@ function makeInfinityAdder() {
   // write code here
   let sum = 0;
 
-  function adder(...args) {
-    if (args.length === 0) {
+  function adder(num) {
+    if (num === undefined) {
       const result = sum;
 
       sum = 0;
 
       return result;
-    } else {
-      sum += args.reduce((acc, num) => acc + num, 0);
-
-      return adder;
     }
+
+    sum += num;
+
+    return adder;
   }
 
   return adder;
