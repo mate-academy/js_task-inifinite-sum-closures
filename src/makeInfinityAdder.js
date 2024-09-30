88ab7830dd34f819fe3ab19a26157e0f4bd8c2d9
Ivan <ivan.kartavy@icloud.com>
2024-09-30 15:00:51 +0300

    task done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  const sumNumber = (number) => {
+    if (number === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += number;
+
+    return sumNumber;
+  };
+
+  return sumNumber;
 }
 
 module.exports = makeInfinityAdder;
