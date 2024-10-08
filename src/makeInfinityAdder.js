a2697b8051c8382d379ee054e3ea2d4b46a5af76
Mykola Zavalnitskyi <zavalnitskiyk@gmaail.com>
2024-10-09 00:21:40 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let resultSum = 0;
+
+  const funAdder = (a) => {
+    if (a === undefined) {
+      const returnValue = resultSum;
+
+      resultSum = 0;
+
+      return returnValue;
+    } else {
+      resultSum += a;
+
+      return funAdder;
+    }
+  };
+
+  return funAdder;
 }
 
 module.exports = makeInfinityAdder;
