9603a39d373527aa71d56ff6b4ea4fdcf6a47b5a
Maxym Modrytskyi <maxmodrr@gmail.com>
2024-10-10 22:54:34 +0300

    result

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sumStart = 0;
+
+  const adder = (num) => {
+    const currentSum = sumStart;
+
+    if (num !== undefined) {
+      sumStart += num;
+
+      return adder;
+    } else {
+      sumStart = 0;
+
+      return currentSum;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
