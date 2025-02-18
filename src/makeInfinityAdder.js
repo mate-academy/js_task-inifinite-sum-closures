8141596141a0edcec88c4a54748897631dab604a
inikiforov <ievgen.nikiforov@tedata.de>
2025-02-18 11:10:41 +0100

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,28 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let summ = 0;
+  let counter = 0;
+  const sumFunc = function (value) {
+    if (counter) {
+      summ = 0;
+
+      return summ;
+    }
+
+    if (!value && value !== 0) {
+      counter++;
+
+      return summ;
+    }
+
+    counter = 0;
+    summ += value;
+
+    return sumFunc;
+  };
+
+  return sumFunc;
 }
 
 module.exports = makeInfinityAdder;
