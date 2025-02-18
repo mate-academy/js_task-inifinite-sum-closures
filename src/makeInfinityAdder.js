3ea83f0f3b56192b09c327953e6422441f470666
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-02-19 00:01:56 +0200

    'Solution'

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,22 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (a) => {
+    if (a === undefined) {
+      const temp = sum;
+
+      sum = 0;
+
+      return temp;
+    }
+    sum += a;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
