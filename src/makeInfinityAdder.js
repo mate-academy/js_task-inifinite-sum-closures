114900e9ef75f5a976ec330dc6f467dc74640680
Aleksandr-front-end-developer <oleksandr.danyliuk.88@gmail.com>
2025-03-11 08:43:27 +0200

    have done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,28 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let first = true;
+  let sum = 0;
+
+  function adder(...args) {
+    if (args.length > 0) {
+      sum += args[0];
+
+      return adder;
+    } else {
+      if (first) {
+        first = false;
+
+        return sum;
+      } else {
+        sum = 0;
+
+        return sum;
+      }
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
