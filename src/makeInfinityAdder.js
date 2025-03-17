ce3d24c2bb25c7a416c022760a867fb1f6a33242
Vova <nechayvova2005@gmail.com>
2025-03-18 00:21:52 +0200

    Implement makeInfinityAdder function to accumulate sums and reset total

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+
+  let total = 0;
+
+  const add = function (num) {
+    if (num === undefined) {
+      const result = total;
+
+      total = 0;
+
+      return result;
+    }
+    total += num;
+
+    return add;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
