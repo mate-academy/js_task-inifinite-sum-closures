dfcc8cdacedea86147f1631f96a0d1422a2bf2bf
Kostiantyn Pavlenko <kostianpv@gmail.com>
2025-03-23 19:30:27 +0200

    solved infinity sum task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,27 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let amount = 0;
+
+  const sum = (firstNum) => {
+    const nextSum = (secondNum) => {
+      return sum(secondNum);
+    }
+
+    if (firstNum !== undefined) {
+      amount += firstNum;
+
+      return nextSum;
+    } else {
+      const returnedAmount = amount;
+
+      amount = 0;
+      
+      return returnedAmount;
+    }
+  }
+
+  return sum;
 }
 
 module.exports = makeInfinityAdder;
