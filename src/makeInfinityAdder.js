9bf0800d4687fc8f406c8be43fc47513e0332e99
Diana <berezandiana24@gmail.com>
2025-03-25 12:14:06 +0100

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function add(value) {
+    if (value !== undefined) {
+      sum += value;
+
+      return add;
+    }
+
+    const result = sum;
+
+    sum = 0;
+
+    return result;
+  }
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
