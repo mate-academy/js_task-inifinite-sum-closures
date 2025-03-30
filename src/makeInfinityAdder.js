5b09e86e8f59b2ee2ded12b6aef11642b5d636ad
Paulo Rodrigues <paulo.jorge.rodrigues@gmail.com>
2025-03-30 22:46:14 +0100

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let balance = 0;
+
+  const adder = (v = undefined) => {
+    if (v === undefined) {
+      const ret = balance;
+
+      balance = 0;
+
+      return ret;
+    } else {
+      balance += v;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
