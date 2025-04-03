4e694178ed9816eb34769399a6c0facaf2f4e3d6
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2025-04-03 18:54:13 +0200

    sol

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,25 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  return function adder(a = 0) {
+    if (!a) {
+      return 0;
+    }
+
+    let sum = a;
+
+    function inner(b) {
+      if (b === undefined) {
+        return sum;
+      }
+
+      sum += b;
+
+      return inner;
+    }
+
+    return inner;
+  };
 }
 
 module.exports = makeInfinityAdder;
