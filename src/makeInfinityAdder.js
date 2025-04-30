89e6b7b5fce36ed02dfe0ab0b850a5fc865b8a1a
Daria <darialys2510@gmail.com>
2025-04-30 15:15:48 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let TOTAL_AMOUNT = 0;
+
+  function ADDER(...args) {
+    if (args.length === 0) {
+      const result = TOTAL_AMOUNT;
+
+      TOTAL_AMOUNT = 0;
+
+      return result;
+    } else {
+      TOTAL_AMOUNT += args[0];
+
+      return ADDER;
+    }
+  }
+
+  return ADDER;
 }
 
 module.exports = makeInfinityAdder;
