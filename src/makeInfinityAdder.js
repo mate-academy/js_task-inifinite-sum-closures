89319c04681bbf875e73842a9308dd341c18891c
Viktoria Zabolotnia <vika.zab4@gmail.com>
2025-05-04 19:38:44 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let totalSum = 0;
+
+  return function adder(num) {
+    if (num === undefined) {
+      const result = totalSum;
+
+      totalSum = 0;
+
+      return result;
+    } else {
+      totalSum += num;
+
+      return adder;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
