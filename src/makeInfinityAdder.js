f03886a30dfc322f9a037bea1d7353ccd7e843b4
Daniel-spec-dell <fvgdanil@gmail.com>
2025-06-02 17:23:59 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let result = 0;
+
+  return function adder(...value) {
+    if (value.length === 0) {
+      const finalResult = result;
+
+      result = 0;
+
+      return finalResult;
+    }
+
+    for (let i = 0; i < value.length; i++) {
+      result += value[i];
+    }
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
