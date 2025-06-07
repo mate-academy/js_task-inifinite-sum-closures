3ee9e281193a505b888629139a51d42febf10815
Maksym Panasovskyi <ie.panasovskyi@gmail.com>
2025-06-07 18:30:07 +0100

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let count = 0;
+  let result = 0;
+
+  return function adder(...args) {
+    if (args.length !== 0) {
+      count += args[0];
+    } else {
+      result = count;
+      count = 0;
+    }
+
+    if (count > 0) {
+      return adder;
+    } else {
+      return result;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
