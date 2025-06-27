cb8224a8341984daabe3453fd8f694fb6e9ac584
michelam-pseudo <michelammilian@outlook.com>
2025-06-27 20:14:40 -0300

    Foo

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,30 +3,27 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   // write code here
   let sum = 0;
   let sumPreClear;
 
   function adder() {
-
-    if(arguments.length === 0) {
-
+    if (arguments.length === 0) {
       sumPreClear = [sum];
       sum = 0;
 
-      return sum;
+      return sumPreClear;
     }
 
-    for(let i = 0; i < arguments.length; i++) {
+    for (let i = 0; i < arguments.length; i++) {
       sum += arguments[i];
     }
 
     return adder;
-  };
+  }
 
   return adder;
-  
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
