bd98cff5ca4f123b898f51ccbb980afe9e6b8361
michelam-pseudo <michelammilian@outlook.com>
2025-06-27 20:07:25 -0300

    Codificação da função makeInfinityAdder'

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,28 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+  let sumPreClear;
+
+  function adder() {
+
+    if(arguments.length === 0) {
+
+      sumPreClear = [sum];
+      sum = 0;
+
+      return sum;
+    }
+
+    for(let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+
+    return adder;
+  };
+
+  return adder;
+  
 }
 
 module.exports = makeInfinityAdder;
