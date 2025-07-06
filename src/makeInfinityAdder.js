40056cabd42d337192aee78edbccb469528f6c2e
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-07-06 12:16:15 +0300

    done function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,32 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+  const adder = (param) => {
+    if (param === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    if (param === 0) {
+      return adder;
+    }
+
+    if (param) {
+      sum += param;
+    }
+
+    return adder;
+  };
+
+  adder.clear = () => {
+    return sum;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
