29c0bb32ea944aaf7d3b307820847428e667c0c8
Nikolay Tyshchenko <nikolay.tyshchenko04@gmail.com>
2025-07-22 15:19:12 +0300

    solution for infinite sum closures task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adderFunction = (...args) => {
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += args[0];
+
+    return adderFunction;
+  };
+
+  return adderFunction;
 }
 
 module.exports = makeInfinityAdder;
