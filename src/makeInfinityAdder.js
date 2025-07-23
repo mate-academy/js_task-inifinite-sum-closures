4d3418899a9c0feae8e8913fdc65fef699fc439a
nikita.lucenko02@gmail.com <nikitalucenko@MacBook-Air-Nikita.local>
2025-07-23 23:34:47 +0300

    closures solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,21 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  return function recurs(a) {
+    if (arguments.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += a;
+
+      return recurs;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
