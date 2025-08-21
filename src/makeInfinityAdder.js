b8efd78548699501923ffd1a7f61b9fa82aa4cf5
Artur Misko <arthurmisko@gmail.com>
2025-08-21 20:00:12 +0200

    done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,28 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  return function adder(x) {
+    let sum = x || 0;
+
+    function inner(y) {
+      if (y === undefined) {
+        const result = sum;
+
+        sum = 0;
+
+        return result;
+      }
+      sum += y;
+
+      return inner;
+    }
+
+    if (x === undefined) {
+      return 0;
+    }
+
+    return inner;
+  };
 }
 
 module.exports = makeInfinityAdder;
