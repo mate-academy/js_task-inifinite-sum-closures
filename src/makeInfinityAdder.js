1d303c1d8f6d8a374500bbc2206bb5fa96f276cb
Yehor Mishchenko <yehormishchenkowork@gmail.com>
2025-09-10 18:01:38 +0200

    added changes

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   // write code here
   let total = 0;
 
@@ -24,4 +24,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
