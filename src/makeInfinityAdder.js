373e35882bf1326897f638c82f18f618e67032d0
Samuel Isaza <samuelisaza08@outlook.com>
2025-09-20 10:19:04 -0500

    Changed function name to makeAdder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let totalSum = 0;
 
   const adder = (...args) => {
@@ -25,4 +25,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
