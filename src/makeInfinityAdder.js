f0f2c8fb5f268fd0b503d76d716d2199b107c741
Karen Behlarian <Gladiatorkaren@gmail.com>
2025-09-22 09:17:03 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,8 +3,18 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
-  // write code here
+function makeInfinityAdder(number) {
+  let sum = number || 0;
+  function adder(x) {
+    if (x === undefined) {
+      const result = sum;
+      sum = 0;
+      return result;
+    }
+    sum += x;
+    return adder;
+  }
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
