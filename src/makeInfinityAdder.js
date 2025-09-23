4ec491405b75e6df78c340d0107363d46de05cad
Artem Shatylo <alukard32244@gmail.com>
2025-09-23 20:42:23 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  return function params(a = null) {
+    if (a === null) {
+      const summ = result;
+
+      result = 0;
+
+      return summ;
+    } else {
+      result += a;
+
+      return params;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
