5570b9cbf73aaa0c7ba793a51357fe8bc235d083
Mariana <mariana.iwaivk@gmail.com>
2025-10-08 20:11:18 -0300

    fix Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let resultOfAdder = 0;
 
   return function adder(a) {
@@ -21,4 +21,4 @@ function makeInfinityAdder() {
   };
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
