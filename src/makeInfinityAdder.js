7cdf186ad71a16e90fe8701160f31a6f876ac47c
Iryna Knyzh <iryna.knyzh@gmail.com>
2025-10-09 13:11:08 +0300

    task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (a) => {
+    if (a === undefined) {
+      const amount = sum;
+
+      sum = 0;
+
+      return amount;
+    }
+
+    sum += a;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
