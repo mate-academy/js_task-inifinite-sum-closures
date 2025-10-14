ede684c11df3c74a2b9b5f890ebb73227810b51e
Paulina <paulina.zwolenik@gmail.com>
2025-10-14 12:20:03 +0200

    Revert "Solution"
    This reverts commit 7e2a2d1a6635dd9cc1c5a965085ac71c6129ac57.

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,23 +4,7 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  let currentSum = 0;
-
-  const adder = (num) => {
-    if (num !== undefined) {
-      currentSum += num;
-
-      return adder;
-    }
-
-    const result = currentSum;
-
-    currentSum = 0;
-
-    return result;
-  };
-
-  return adder;
+  // write code here
 }
 
 module.exports = makeInfinityAdder;
