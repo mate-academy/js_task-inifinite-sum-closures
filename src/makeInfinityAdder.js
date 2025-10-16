a7c9d30fd83a34b88f6579ff82e8e74b7e4c12c5
Elber-Kevenny <vieirabeb@gmail.com>
2025-10-15 22:00:13 -0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  const adder = (add) => {
+    if (typeof add !== 'undefined') {
+      sum += add;
+
+      return adder;
+    } else {
+      const temp = sum;
+
+      sum = 0;
+
+      return temp;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
