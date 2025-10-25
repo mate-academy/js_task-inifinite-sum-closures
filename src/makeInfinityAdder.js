ca63b572974b66136767bc66b2f3d5dc82551f45
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-10-25 18:57:11 +0300

    test passed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  const adder = (number) => {
+    if (typeof number === 'undefined') {
+      const localSum = sum;
+
+      sum = 0;
+
+      return localSum;
+    }
+    sum += number;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
