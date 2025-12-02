cbe92ee38e50140e16cbc3e3f524f2459994135a
Denys Piddubnyi <iforgotmypincode@gmail.com>
2025-12-02 11:43:36 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let count = 0;
 
   const adder = function (a) {
@@ -23,4 +23,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
