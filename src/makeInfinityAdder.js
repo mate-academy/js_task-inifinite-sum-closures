1693f033ae5c69ae39e35170b9949f444b5698de
maksbtw <maks.20050817@gmail.com>
2025-12-09 10:49:42 +0100

    ready to check

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(number) {
+    if (number === undefined) {
+      const buffer = sum;
+
+      sum = 0;
+
+      return buffer;
+    }
+
+    sum += number;
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
