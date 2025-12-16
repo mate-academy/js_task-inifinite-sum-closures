4dbcbd2f9a078e612cf958aecc495c57e954fef0
Nazar Kundys <nazarrisd@gmail.com>
2025-12-16 17:46:47 +0200

    feat: ✨ implemented infinity adder function with cumulative sum logic

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const add = (n) => {
+    if (n == null) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += n;
+
+    return add;
+  };
+
+  return add;
 }
 
 module.exports = makeInfinityAdder;
