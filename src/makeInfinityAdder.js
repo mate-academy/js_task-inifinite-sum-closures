875a50415e90516206fa7e3df9ec2745cd86cb63
Anna Rodina <tavoshka@gmail.com>
2025-12-21 21:49:10 +0200

    fixed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let sum = 0;
   const adder = (num) => {
     if (typeof num === 'undefined') {
@@ -21,4 +21,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
