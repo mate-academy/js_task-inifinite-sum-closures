6fceba836124fcd69d63b855decca4c5bbb1f415
Ihor Haidai <igayday74@gmail.com>
2026-01-02 11:26:45 -0500

    Solution

---
+++ src/makeAdder.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/***
+ * @return {function}
+ */
+function makeAdder() {
+  let sum = 0;
+
+  function adder(value) {
+    if (value === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += value;
+
+    return adder;
+  }
+
+  return adder;
+}
+
+module.exports = makeAdder;
