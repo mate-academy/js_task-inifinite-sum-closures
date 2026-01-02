088abd5d8bc4e259f84fb085586db75c7cf169bb
DevT <dev.tymofiichuk@gmail.com>
2026-01-02 11:59:04 +0200

    Hellish challenge task

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,8 +3,26 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
-  // write code here
+function makeAdder() {
+  let sum = 0;
+
+  const adder = (n) => {
+    if (typeof n === 'undefined') {
+      const r = sum;
+
+      sum = 0;
+
+      return r;
+    }
+
+    if (typeof n === 'number') {
+      sum += n;
+    }
+
+    return adder;
+  };
+
+  return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
