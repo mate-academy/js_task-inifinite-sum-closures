94c27e0e9ea89ce0b6989e14161597a4b53e9b96
Svystun Pavlo <spo26081985@gmail.com>
2026-01-26 19:19:52 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,8 +3,23 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   // write code here
+  let sum = 0;
+
+  return function adder(a) {
+    const sumCopy = sum;
+
+    if (a === undefined) {
+      sum = 0;
+
+      return sumCopy;
+    } else {
+      sum += a;
+
+      return adder;
+    }
+  };
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
