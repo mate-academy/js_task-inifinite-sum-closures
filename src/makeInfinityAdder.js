1021a7735653ca1967ac7503afde11a89d921800
Roman Bilichenko <romabilichenko1@gmail.com>
2026-01-28 16:09:56 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,8 +3,22 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
-  // write code here
+function makeAdder() {
+  let totalSum = 0;
+
+  return function adder(num) {
+    const copyTotalSum = totalSum;
+
+    if (num === undefined) {
+      totalSum = 0;
+
+      return copyTotalSum;
+    } else {
+      totalSum += num;
+
+      return adder;
+    }
+  };
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
