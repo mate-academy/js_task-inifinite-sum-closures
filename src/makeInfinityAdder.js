3739b6fcf794227b0af1c3b3a689684fcbe73dd2
Valentyna <pr.valya1989@gmail.com>
2026-02-03 13:05:23 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const makeAdder = function (numb) {
+    if (numb !== undefined) {
+      result += numb;
+
+      return makeAdder;
+    } else if (numb === undefined) {
+      const temp = result;
+
+      result = 0;
+
+      return temp;
+    }
+  };
+
+  return makeAdder;
 }
 
 module.exports = makeInfinityAdder;
