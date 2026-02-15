f4bf5093ab8f3a73f1a6d1ef9b9fb2902de30f26
Mykola Novik <nik.novik48@gmail.com>
2026-02-15 17:58:57 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let result = 0;
+
+  const adder = (...arg) => {
+    if (arg.length === 0) {
+      const resultForReturn = result;
+
+      result = 0;
+
+      return resultForReturn;
+    }
+
+    result += arg[0];
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
