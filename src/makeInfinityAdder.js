17e64f07f412580662fa44752f7d45fad7bb1d3c
ikseloi <gdalexgo@gmail.com>
2026-03-04 18:03:30 +0200

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,29 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (...args) => {
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    args.forEach((el) => {
+      const n = Number(el);
+
+      if (Number.isFinite(n)) {
+        sum += n;
+      }
+    });
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
