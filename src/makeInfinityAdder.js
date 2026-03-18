c1d6782e9b0481e529c082a5744e5631944d92b2
Sergey Ivanov <ivanovsereza26@gmail.com>
2026-03-18 18:08:47 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,23 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeInfinityAdder(a) {
+  let total = a ?? 0;
+
+  function innerSum(b) {
+    if (arguments.length === 0) {
+      const result = total;
+
+      total = 0;
+
+      return result;
+    }
+    total += b;
+
+    return innerSum;
+  }
+
+  return innerSum;
   // write code here
 }
 
