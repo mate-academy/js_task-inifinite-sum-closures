926929acb04bacea6457248b5be7601c96553b02
Ihor Tutak <igor2tak2212@gmail.com>
2026-03-19 19:31:32 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,21 +3,21 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
-  let sum = 0;
+function makeAdder() {
+  let count = 0;
 
   function adder(...args) {
     if (args.length === 0) {
-      const result = sum;
+      const result = count;
 
-      sum = 0;
+      count = 0;
 
       return result;
     }
 
-    const count = args.reduce((num1, num2) => num1 + num2, 0);
+    const sum = args.reduce((num1, num2) => num1 + num2, 0);
 
-    sum += count;
+    count += sum;
 
     return adder;
   }
@@ -25,4 +25,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
