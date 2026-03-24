a747a60acc6ebfda64562719eab377f0a1ac499d
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-03-24 12:04:01 +0100

    create adder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function next(nextValue) {
+    if (arguments.length > 0) {
+      sum += nextValue;
+
+      return next;
+    }
+
+    const finalSum = sum;
+
+    sum = 0;
+
+    return finalSum;
+  }
+
+  return next;
 }
 
 module.exports = makeInfinityAdder;
