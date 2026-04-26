64ceab584a9b83c843f2b9679948957cd0317ee6
Ihor Balakhmei <ihor@MacBook-Pro-Ihor.local>
2026-04-26 21:45:44 +0200

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,26 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const result = (...args) => {
+    if (args.length === 0) {
+      const currentSum = sum;
+
+      sum = 0;
+
+      return currentSum;
+    }
+
+    sum += args.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0,
+    );
+
+    return result;
+  };
+
+  return result;
 }
 
 module.exports = makeInfinityAdder;
