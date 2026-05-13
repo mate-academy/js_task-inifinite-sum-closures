17c56ec8acd32f50a245d790e3316d7c25e78649
Mauro Yuzo <mauroyuzof@gmail.com>
2026-05-13 00:25:38 -0300

    result

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,22 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let total = 0;
+  const adder = (...args) => {
+    if (args.length === 0) {
+      const result = total;
+
+      total = 0;
+
+      return result;
+    } else {
+      total += args[0];
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
