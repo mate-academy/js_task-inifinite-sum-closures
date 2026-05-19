a87e9530865b9aa7b3f6b4674a873a4c95e6777b
VoloGor <goralvv@gmail.com>
2026-05-19 12:41:15 +0200

    Hellish challenge

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  function adder(num) {
+    if (num === undefined) {
+      const newTotal = total;
+
+      total = 0;
+
+      return newTotal;
+    }
+
+    total += num;
+
+    return adder;
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
