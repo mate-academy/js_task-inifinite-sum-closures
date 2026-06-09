337c82f13243eb521937a0b5539f3e10470bf25f
ptrodion <rdnpetrenko@gmail.com>
2026-06-09 21:25:06 +0100

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,37 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const add = (a) => {
+    if (a === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += a;
+
+    return add;
+  };
+
+  return add;
 }
+
+// eslint-disable-next-line no-undef
+const adder = makeInfinityAdder();
+
+// eslint-disable-next-line no-console
+console.log(
+  adder(), // 0
+  adder(4)(5)(), // 9
+  adder(), // 0
+  adder(5)(5)(5),
+  adder(4),
+  adder(), // 19
+  adder(), // 0
+);
 
 module.exports = makeInfinityAdder;
