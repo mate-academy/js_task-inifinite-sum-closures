63710e87903a7eeddaa5f50014f53055a5f1d74a
Petrenko Oleksii <petrenko1694@gmail.com>
2026-07-08 16:15:03 +0300

    InfinityAdder

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let count = 0;
   let result = 0;
 
@@ -20,4 +20,4 @@ function makeInfinityAdder() {
   };
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
