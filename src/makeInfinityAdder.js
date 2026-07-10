4534a66527046db44a922a513fd24383ef704b41
ALex <sasha.hivuk@gmail.com>
2026-07-10 15:44:10 +0300

    change name function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   // write code here
   let sum = 0;
 
@@ -24,4 +24,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
