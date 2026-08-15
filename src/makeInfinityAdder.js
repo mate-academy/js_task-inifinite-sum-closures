fe8a53a6d95473948ce3ca49b4cae38a3e836d02
Yevhenii Dmytriienko <dmitrik24042007@gmail.com>
2026-08-15 14:49:47 +0300

    fix function name

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let sum = 0;
 
   return function adder(num) {
@@ -21,4 +21,4 @@ function makeInfinityAdder() {
   };
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
