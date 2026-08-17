0ab7fa36f3efb2f799301e6ed7d6e4e3a94d1135
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-08-17 12:13:02 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,22 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const addFunc = (number) => {
+    if (number === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+    sum += number;
+
+    return addFunc;
+  };
+
+  return addFunc;
 }
 
 module.exports = makeInfinityAdder;
