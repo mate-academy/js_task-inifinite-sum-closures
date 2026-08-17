b0589c3a7c8e6a4a3bab1a519d256c59bed51ee9
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2026-08-17 13:07:29 +0300

    Completed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,8 +6,10 @@
 function makeInfinityAdder() {
   let sum = 0;
 
-  function adderNum(num) {
-    if (num === undefined) {
+  function adder(num = 0) {
+    sum += num;
+
+    if (num === 0) {
       const result = sum;
 
       sum = 0;
@@ -15,12 +17,10 @@ function makeInfinityAdder() {
       return result;
     }
 
-    sum += num;
-
-    return adderNum;
+    return adder;
   }
 
-  return adderNum;
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
