1662df7f80df33c12bd6d7a4ae7dd2e67137d620
Ruslan Sydorenko <letovletov111@gmail.com>
2026-08-19 19:06:31 +0200

    task done

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let counter = 0;
+
+  return function adder(number) {
+    if (number || number === 0) {
+      counter += number;
+
+      return adder;
+    }
+
+    const result = counter;
+
+    counter = 0;
+
+    return result;
+  };
 }
 
 module.exports = makeInfinityAdder;
