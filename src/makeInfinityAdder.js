20acb1f9028ee53d50a23c2fa65a1c507132db4c
Jeverson Alves Borges <Jeversonborges@icloud.com>
2026-08-25 22:04:28 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,9 +1,9 @@
 'use strict';
 
-/***
+/**
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let sum = 0;
 
   function adder(number) {
@@ -23,4 +23,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
